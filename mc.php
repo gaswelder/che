27c8b80667d7933773076bf607700ece93502ded
gas <gasweld@gmail.com>
2016-09-22 03:08:54 +0300

    Compiler command change

--- mc.php
+++ mc.php
@@ -82,8 +82,8 @@ function tmppath($path)
 
 function c99($sources, $name)
 {
-	$cmd = 'c99 -Wall -Wextra -pedantic -pedantic-errors';
-	$cmd .= ' -fmax-errors=10 -D _XOPEN_SOURCE=700';
+	$cmd = 'c99 -Wall -Wextra -Werror -pedantic -pedantic-errors';
+	$cmd .= ' -fmax-errors=3 -D _XOPEN_SOURCE=700';
 	$cmd .= ' -g ' . implode(' ', $sources);
 	$cmd .= ' -o '.$name;
 	exec($cmd, $output, $ret);
