9d3f8b9cc3ed0c065ae67bc7a886e44417db0baf
gas <gasweld@gmail.com>
2016-09-22 03:08:30 +0300

    clip/map lib

---
+++ lib/clip/map.c
@@ -0,0 +1,209 @@
+import "cli"
+/*
+ */
+
+#define MAXKEY 256
+
+/*
+ * The map
+ */
+struct __map {
+	struct __table *t;
+};
+
+struct __table {
+	int nitems; // how many items
+	int nbuckets; // how many buckets
+	struct __item **buckets;
+};
+
+/*
+ * An element of the hash table
+ */
+struct __item {
+	struct __item *next;
+	char key[MAXKEY];
+	void *value;
+	int intval;
+};
+
+typedef struct __map map;
+
+map *map_new()
+{
+	map *m = alloc(1, sizeof(*m));
+	m->t = create_table(16);
+	return m;
+}
+
+void map_free(map *m)
+{
+	free_table(m->t);
+	free(m);
+}
+
+/*
+ * Returns true if given key exists
+ */
+bool map_exists(map *m, const char *key)
+{
+	struct __item *i = find(m->t, key);
+	return i ? true : false;
+}
+
+/*
+ * Returns value from given key
+ */
+void *map_get(map *m, const char *key)
+{
+	struct __item *i = find(m->t, key);
+	return i->value;
+}
+
+int map_geti(map *m, const char *key)
+{
+	struct __item *i = find(m->t, key);
+	return i->intval;
+}
+
+/*
+ * Assigns value to given key
+ */
+void map_set(map *m, const char *key, void *val)
+{
+	struct __item *i = find(m->t, key);
+	/*
+	 * If already exists, just change the value.
+	 */
+	if(i) {
+		i->value = val;
+		return;
+	}
+	/*
+	 * Grow the table if needed and insert new item
+	 */
+	if(m->t->nitems / m->t->nbuckets > 10) {
+		grow(m);
+	}
+	insert(m->t, key, val, 0);
+}
+
+void map_seti(map *m, const char *key, int val)
+{
+	struct __item *i = find(m->t, key);
+	if(i) {
+		i->intval = val;
+		return;
+	}
+	if(m->t->nitems / m->t->nbuckets > 10) {
+		grow(m);
+	}
+	insert(m->t, key, NULL, val);
+}
+
+// ---
+
+/*
+ * Grows the table
+ */
+static void grow(map *m)
+{
+	struct __table *old = m->t;
+
+	/*
+	 * Create new table and reinsert values into it
+	 */
+	struct __table *new = create_table(old->nbuckets * 2);
+	int j;
+	for(j = 0; j < old->nbuckets; j++) {
+		struct __item *i = old->buckets[j];
+		while(i) {
+			insert(new, i->key, i->value, i->intval);
+			i = i->next;
+		}
+	}
+
+	free_table(old);
+	m->t = new;
+}
+
+/*
+ * Finds an item in a single bucket
+ */
+static struct __item *find(struct __table *t, const char *key)
+{
+	struct __item *i = t->buckets[hash(key) % t->nbuckets];
+	while(i) {
+		if(strcmp(i->key, key) == 0) {
+			return i;
+		}
+		i = i->next;
+
+	}
+	return NULL;
+}
+
+static void insert(struct __table *t, const char *key, void *val, int intval)
+{
+	if(strlen(key) >= MAXKEY) {
+		fatal("Key too long: %s", key);
+	}
+
+	struct __item *i = alloc(1, sizeof(*i));
+	strcpy(i->key, key);
+	i->value = val;
+	i->intval = intval;
+
+	int pos = hash(key) % t->nbuckets;
+	struct __item *b = t->buckets[pos];
+	i->next = b;
+	t->buckets[pos] = i;
+
+	t->nitems++;
+}
+
+static struct __table *create_table(int size)
+{
+	struct __table *t = alloc(1, sizeof(*t));
+	t->buckets = alloc(size, sizeof(struct __bucket *));
+	t->nbuckets = size;
+	return t;
+}
+
+static void free_table(struct __table *t)
+{
+	int k;
+	for(k = 0; k < t->nbuckets; k++) {
+		struct __item *i;
+		struct __item *next;
+		i = t->buckets[k];
+		while(i) {
+			next = i->next;
+			free(i);
+			i = next;
+		}
+	}
+	free(t);
+}
+
+/*
+ * Home-made hash function for strings
+ */
+static int hash(const char *s)
+{
+	unsigned long h = 0;
+	while(*s) {
+		h = (h * *s  + 2020181) % 2605013;
+		s++;
+	}
+	return h;
+}
+
+static void *alloc(size_t n, size_t size)
+{
+	void *m = calloc(n, size);
+	if(!m) {
+		fatal("Couldn't allocate %zux%zu bytes", n, size);
+	}
+	return m;
+}
---
+++ test/wc.c
@@ -0,0 +1,57 @@
+import "lib/prng/lcg"
+import "lib/clip/map"
+
+int main()
+{
+	int x = 42;
+	lcg_seed(445, 700001, 2097152, x);
+
+	map *h = map_new();
+
+	int max = 0;
+	char maxword[7];
+	int i;
+	for(i = 0; i < 900000; i++)
+	{
+		char word[7];
+		genword(word, 7);
+
+		int c;
+		if(!map_exists(h, word)) {
+			c = 0;
+		}
+		else {
+			c = map_geti(h, word);
+		}
+
+		c++;
+		map_seti(h, word, c);
+
+		if(c > max) {
+			strcpy(maxword, word);
+			max = c;
+		}
+	}
+
+	map_free(h);
+
+	puts(maxword);
+	return 0;
+}
+
+static void genword(char *buffer, int size)
+{
+	static char *consonants = "bcdfghjklmnprstvwxz";
+	static char *vowels = "aeiou";
+	int i;
+
+	for(i = 0; i < size-1; i++)
+	{
+		if(i % 2 == 0) {
+			buffer[i] = consonants[ lcg_rand() % 19 ];
+		} else {
+			buffer[i] = vowels[ lcg_rand() % 5 ];
+		}
+	}
+	buffer[i] = '\0';
+}
