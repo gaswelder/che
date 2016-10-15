e7fbb8dd41eb49ef46254763674b2df025304fcb
gas <gasweld@gmail.com>
2016-10-15 18:11:29 +0300

    bitreader lib

---
+++ lib/bitreader.c
@@ -0,0 +1,63 @@
+
+struct bitstream {
+	FILE *f;
+	uint8_t byte;
+	int bytepos;
+};
+
+typedef struct bitstream bits_t;
+
+/*
+ * Returns new bitreader for file 'f'.
+ */
+bits_t *bits_new(FILE *f)
+{
+	bits_t *b = calloc(1, sizeof(bits_t));
+	if(!b) return NULL;
+	b->f = f;
+	b->bytepos = -1;
+	return b;
+}
+
+/*
+ * Frees memory used by the reader.
+ */
+void bits_free(bits_t *b)
+{
+	free(b);
+}
+
+/*
+ * Returns value of the next bit.
+ * Returns -1 if there is no next bit.
+ */
+int bits_get(bits_t *s)
+{
+	if(s->bytepos < 0) {
+		int c = fgetc(s->f);
+		if(c == EOF) return -1;
+		s->byte = (uint8_t) c;
+		s->bytepos = 7;
+	}
+	int b = (s->byte >> s->bytepos) & 1;
+	s->bytepos--;
+	return b;
+}
+
+/*
+ * Returns value of next 'n' bits.
+ * Returns -1 if there is not enough bits in the stream.
+ */
+int bits_getn(bits_t *s, int n)
+{
+	int r = 0;
+	for(int i = 0; i < n; i++) {
+		int c = bits_get(s);
+		if(c < 0) {
+			return -1;
+		}
+		r *= 2;
+		r += c;
+	}
+	return r;
+}
---
+++ test/bits.c
@@ -0,0 +1,27 @@
+import "bitreader"
+import "cli"
+
+/*
+ * Reads and prints a file using the bitreader
+ * to read characters.
+ */
+int main(int argc, char *argv[])
+{
+	if(argc != 2) {
+		fatal("Usage: bits <textfile>");
+	}
+
+	FILE *f = fopen(argv[1], "rb");
+	bits_t *b = bits_new(f);
+
+	while(1) {
+		int c = bits_getn(b, 8);
+		if(c < 0) {
+			break;
+		}
+		printf("%c", c);
+	}
+
+	bits_free(b);
+	fclose(f);
+}
