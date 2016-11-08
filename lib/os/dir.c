74979f959a3e113479b5e3027175c6312060e693
gas <gasweld@gmail.com>
2016-11-08 20:30:15 +0300

    os/dir lib for directories

---
+++ lib/os/dir.c
@@ -0,0 +1,46 @@
+#include <dirent.h>
+#type DIR
+
+struct __dir {
+	DIR *d;
+};
+
+typedef struct __dir dir_t;
+
+/*
+ * Opens directory at given path and returns its handle.
+ * Returns NULL on failure.
+ */
+pub dir_t *diropen(const char *path)
+{
+	dir_t *d = calloc(1, sizeof(struct __dir));
+	if(!d) return NULL;
+
+	d->d = opendir(path);
+	if(!d->d) {
+		free(d);
+		return NULL;
+	}
+
+	return d;
+}
+
+/*
+ * Moves to next file in the directory and returns
+ * its name.
+ */
+pub const char *dirnext(dir_t *d)
+{
+	struct dirent *e = readdir(d->d);
+	if(!e) return NULL;
+	return e->d_name;
+}
+
+/*
+ * Closes the directory handle/
+ */
+pub void dirclose(dir_t *d)
+{
+	closedir(d->d);
+	free(d);
+}
---
+++ test/dir.c
@@ -0,0 +1,34 @@
+import "os/dir"
+
+int main(int argc, char *argv[])
+{
+	if(argc > 2) {
+		fprintf(stderr, "Usage: dir [path]\n");
+		return 1;
+	}
+
+	const char *path;
+	if(argc > 1) {
+		path = argv[1];
+	}
+	else {
+		path = ".";
+	}
+
+	dir_t *d = diropen(path);
+	if(!d) {
+		fprintf(stderr, "Couldn't read %s\n", path);
+		return 1;
+	}
+
+	defer dirclose(d);
+
+	while(1) {
+		const char *fn = dirnext(d);
+		if(!fn) break;
+
+		printf("%s\n", fn);
+	}
+
+	return 0;
+}
