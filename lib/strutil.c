d62a9de4032f7199e8c2fdbd0832c7dc135035f7
gas <gasweld@gmail.com>
2016-11-08 21:56:19 +0300

    strutil lib

---
+++ lib/strutil.c
@@ -0,0 +1,22 @@
+/*
+ * Creates a new formatted string in memory.
+ */
+pub char *newstr(const char *format, ... )
+{
+	va_list args;
+	
+	va_start(args, format);
+	int len = vsnprintf(NULL, 0, format, args);
+	va_end(args);
+	
+	if(len < 0) return NULL;
+	
+	char *buf = malloc(len + 1);
+	if(!buf) return NULL;
+	
+	va_start(args, format);
+	vsnprintf(buf, len + 1, format, args);
+	va_end(args);
+	
+	return buf;
+}
