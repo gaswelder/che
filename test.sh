ad367dac001e83c3443858b23218da9163c7b257
gas <gasweld@gmail.com>
2017-12-02 04:06:51 +0300

    Build tests before samples

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ fi
 
 cd bin
 
+for i in ../test/*.c ../prog/*.c; do
+	basename $i
+	CHE_HOME=.. php ../che "$i" || exit 1
+done
+
 for i in ../prog/*; do
 	if [ ! -d "$i" ]; then continue; fi
 	name=`basename "$i"`
 	echo $name
 	CHE_HOME=.. php ../che "$i/$name.c" || exit 1
-done
-
-for i in ../test/*.c ../prog/*.c; do
-	basename $i
-	CHE_HOME=.. php ../che "$i" || exit 1
 done
