b3e753b9ea3259a42b9c37e8bf97ecddffa6d2bc
gasweld <gasweld@gmail.com>
2019-01-10 23:22:15 +0300

    Cleaner operator formatting

--- mc/nodes/c_binary_op.php
+++ mc/nodes/c_binary_op.php
@@ -15,6 +15,22 @@ class c_binary_op
 
     function format()
     {
-        return sprintf('(%s) %s (%s)', $this->a->format(), $this->op, $this->b->format());
+        $parts = [
+            $this->brace_if_needed($this->a),
+            $this->op,
+            $this->brace_if_needed($this->b)
+        ];
+        if (in_array($this->op, ['.', '->'])) {
+            return implode('', $parts);
+        }
+        return implode(' ', $parts);
+    }
+
+    private function brace_if_needed($operand)
+    {
+        if ($operand instanceof c_binary_op && operator_strength($operand->op) < operator_strength($this->op)) {
+            return '(' . $operand->format() . ')';
+        }
+        return $operand->format();
     }
 }
