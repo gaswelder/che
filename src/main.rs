6feec4570eb70bc8bc947fabdcd08e189ae587b1
gasweld <gasweld@gmail.com>
2021-01-16 23:42:35 +0300

    Clean up function dispatch

--- src/main.rs
+++ src/main.rs
@@ -13,52 +13,48 @@ fn main() {
         let v: Value = serde_json::from_str(&buf).unwrap();
         let f = v.as_object().unwrap().get("f").unwrap().as_str().unwrap();
         let args = v.as_object().unwrap().get("a").unwrap().as_array().unwrap();
-        if f == "echo" {
-            let s = args[0].as_str().unwrap();
-            println!(
-                "{}",
-                json!({
-                    "error": "",
-                    "data": echo(s)
-                })
-            )
-        } else if f == "is_op" {
-            let token_type = args[0].as_str().unwrap();
-            println!(
-                "{}",
-                json!({
-                    "error": "",
-                    "data": parser::is_op(String::from(token_type))
-                })
-            )
-        } else if f == "operator_strength" {
-            let op = args[0].as_str().unwrap();
-            println!(
-                "{}",
-                json!({
-                    "error": "",
-                    "data": parser::operator_strength(String::from(op))
-                })
-            )
-        } else if f == "is_postfix_op" {
-            let op = args[0].as_str().unwrap();
-            println!(
-                "{}",
-                json!({
-                    "error": "",
-                    "data": parser::is_postfix_op(String::from(op))
-                })
-            )
-        } else {
-            println!(
-                "{}",
-                json!({
-                    "error": "unknown function",
-                    "data": null
-                })
-            )
-        }
+        route(f, args);
     }
+}
+
+fn route(f: &str, args: &Vec<serde_json::Value>) {
+    let result = match f {
+        "echo" => {
+            let s = args[0].as_str().unwrap();
+            json!({
+                "error": "",
+                "data": echo(s)
+            })
+        }
+        "is_op" => {
+            let token_type = args[0].as_str().unwrap();
+            json!({
+                "error": "",
+                "data": parser::is_op(String::from(token_type))
+            })
+        }
+        "operator_strength" => {
+            let op = args[0].as_str().unwrap();
+            json!({
+                "error": "",
+                "data": parser::operator_strength(String::from(op))
+            })
+        }
+        "is_postfix_op" => {
+            let op = args[0].as_str().unwrap();
+            json!({
+                "error": "",
+                "data": parser::is_postfix_op(String::from(op))
+            })
+        }
+        _ => {
+            json!({
+                "error": "unknown function",
+                "data": null
+            })
+        }
+    };
+    println!("{}", result);
 }
 
 fn echo(s: &str) -> String {
