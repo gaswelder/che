23559add8302fbfaf837a4bcdd4f757cb0703482
gasweld <gasweld@gmail.com>
2021-01-17 00:54:11 +0300

    Add object proxying to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,59 @@
 use serde_json::{json, Value};
+use std::env;
 use std::io::stdin;
 use std::string::String;
 mod parser;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        serve_class(args[1].to_string());
+    } else {
+        serve_functions();
+    }
+}
+
+fn serve_class(class_name: String) {
+    if class_name != "buf" {
+        panic!("unknown class name: {}", class_name);
+    }
+    let mut constructed = false;
+    let mut s = String::new();
+
+    loop {
+        let mut buf = String::new();
+        stdin().read_line(&mut buf).unwrap();
+        if buf == "" {
+            break;
+        }
+        let v: Value = serde_json::from_str(&buf).unwrap();
+        let f = v.as_object().unwrap().get("f").unwrap().as_str().unwrap();
+        let args = v.as_object().unwrap().get("a").unwrap().as_array().unwrap();
+        if !constructed {
+            if f != "__construct" {
+                panic!("__construct call expected");
+            }
+            s = args[0].as_str().unwrap().to_string();
+            constructed = true;
+            let response = json!({
+                "error": "",
+                "data": null
+            });
+            println!("{}", response);
+        } else {
+            if f != "len" {
+                panic!("unknown method: {}", f);
+            }
+            let response = json!({
+                "error": "",
+                "data": s.len()
+            });
+            println!("{}", response);
+        }
+    }
+}
+
+fn serve_functions() {
     loop {
         let mut buf = String::new();
         stdin().read_line(&mut buf).unwrap();
