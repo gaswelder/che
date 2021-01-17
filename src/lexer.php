1d008869ce0701ab91bfa8c1b5bcfaa075c4b52f
gasweld <gasweld@gmail.com>
2021-01-18 00:00:23 +0300

    Move functions

--- src/lexer.php
+++ src/lexer.php
@@ -91,6 +91,33 @@ function error_token($buf, $msg)
 	return make_token('error', $msg, $buf->pos());
 }
 
+function make_token($type, $data, $pos)
+{
+	return [
+		'type' => $type,
+		'content' => $data,
+		'pos' => $pos
+	];
+}
+
+function token_to_string($token)
+{
+	if ($token['content'] === null) {
+		return '[' . $token['type'] . ']';
+	}
+
+	$n = 40;
+	if (mb_strlen($token['content']) > $n) {
+		$c = mb_substr($token['content'], 0, $n - 3) . '...';
+	} else $c = $token['content'];
+	$c = str_replace(array("\r", "\n", "\t"), array(
+		"\\r",
+		"\\n",
+		"\\t"
+	), $c);
+	return "[$token[type], $c]";
+}
+
 const spaces = "\r\n\t ";
 
 function read_token($buf)
