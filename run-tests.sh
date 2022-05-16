8d5831a8d10e29787fea8f1023bfa14198ab8fcb
gasweld <gasweld@gmail.com>
2022-05-16 04:13:56 +0400

    Fix tests runner

--- run-tests.sh
+++ run-tests.sh
@@ -4,8 +4,10 @@ if [ ! -d bin ]; then
 	mkdir bin || exit 1
 fi
 
+cargo build || exit 1
+
 for i in prog/*.c; do
 	name=`basename $i`
 	echo $name
-	cargo run build "$i" "bin/$name" || exit 1
+	target/debug/che build "$i" "bin/$name" || exit 1
 done
