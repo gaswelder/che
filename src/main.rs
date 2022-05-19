492f5cf00cd4022f5f6896129d7fb9d1cb3c8554
gasweld <gasweld@gmail.com>
2022-05-20 01:24:57 +0400

    Rewrite deptree in rust

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ fn main() {
     if args[1] == "build" {
         exit(build(&args[2..]));
     }
+    if args[1] == "deptree" {
+        exit(deptree(&args[2..]));
+    }
     exit(1);
 }
 
@@ -129,4 +132,61 @@ fn module_imports(module: &nodes::Module) -> Vec<nodes::Import> {
         }
     }
     return list;
+}
+
+fn deptree(argv: &[String]) -> i32 {
+    if argv.len() != 1 {
+        eprintln!("usage: deptree <source-path>");
+        return 1;
+    }
+    let path = &argv[0];
+    let m = parser::get_module(&path).unwrap();
+    let tree = build_tree(&m);
+    let r = render_tree(&tree);
+    println!("{}", r.join("\n"));
+    return 0;
+}
+
+struct DepNode {
+    name: String,
+    deps: Vec<DepNode>,
+}
+
+fn build_tree(module: &nodes::Module) -> DepNode {
+    let imports = module_imports(module);
+    let mut deps = vec![];
+    for import in imports {
+        let m = parser::get_module(&import.path).unwrap();
+        deps.push(build_tree(&m));
+    }
+    return DepNode {
+        name: module.id.clone(),
+        deps,
+    };
+}
+
+fn render_tree(tree: &DepNode) -> Vec<String> {
+    let mut lines: Vec<String> = vec![tree.name.clone()];
+    let n = tree.deps.len();
+    for (i, dep) in tree.deps.iter().enumerate() {
+        let r = render_tree(&dep);
+        if i == n - 1 {
+            lines.append(&mut indent_tree(r, " └", "  "));
+        } else {
+            lines.append(&mut indent_tree(r, " ├", " │"));
+        }
+    }
+    return lines;
+}
+
+fn indent_tree(lines: Vec<String>, first: &str, cont: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for (i, line) in lines.iter().enumerate() {
+        if i == 0 {
+            result.push(format!("{}{}", first, line))
+        } else {
+            result.push(format!("{}{}", cont, line));
+        }
+    }
+    return result;
 }
