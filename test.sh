eeac26dd5360e4bf6af62b5b9061527496b62788
gasweld <gasweld@gmail.com>
2022-05-31 00:15:33 +0400

    Remove times.txt

--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-/usr/bin/time -f "%x\t%M\t%S\t%U" -a -o "times.txt" ./run-tests.sh
+cargo test || exit $?
+
+if [ ! -d bin ]; then
+	mkdir bin || exit 1
+fi
+
+cargo build || exit 1
+
+for i in prog/*.c; do
+	name=`basename $i .c`
+	echo $name
+	target/debug/che build "$i" "prog/$name.out"
+	if [ -f "prog/$name.test.sh" ]; then
+		cd prog
+		./$name.test.sh && echo "OK $name" || exit 1
+		cd ..
+	fi
+done
+
+rm prog/*.out
