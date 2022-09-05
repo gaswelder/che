ca5bf278f2208ed569e02ec13f474026e97b1aef
gasweld <gasweld@gmail.com>
2022-09-06 02:59:18 +0400

    Add time functions

--- lib/os/misc.c
+++ lib/os/misc.c
@@ -6,13 +6,29 @@
 #include <sys/termios.h>
 #include <sys/mman.h>
 
-typedef struct timeval _timeval;
+pub typedef struct timeval timeval_t;
 
 pub bool stdin_has_input() {
     fd_set readfds = {0};
     FD_ZERO(&readfds);
     FD_SET(STDIN_FILENO, &readfds);
 
-    _timeval timeout = {0};
+    timeval_t timeout = {0};
     return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+/*
+ * Returns current system time as an opaque object.
+ */
+pub timeval_t get_time() {
+    timeval_t time;
+    gettimeofday(&time,0);
+    return time;
+}
+
+/*
+ * Returns unix timestamp in microseconds for the given time object.
+ */
+pub int32_t time_usec(timeval_t *t) {
+    return t->tv_sec * 1000000L + t->tv_usec;
 }
