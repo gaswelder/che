e350737059bb16c06d608230ff3fbc34e9af0978
gasweld <gasweld@gmail.com>
2022-09-14 23:10:26 +0400

    Inline function

--- unix.c
+++ unix.c
@@ -834,7 +834,164 @@ void GenSubtree(FILE *out, ObjDesc *od)
     
     od->flag++;
 
-    if (GenContents(out, od->id) && (od->elm[0].id != 0)) {
+    int r;
+    int result = 1;
+    switch(od->id) {
+        case CITY:
+            xmlprintf(out, "%s", cities[ignuin(0,cities_len-1)]);
+            break;
+        case TYPE:
+            xmlprintf(out, "%s", GenContents_auction_type[ignuin(0,1)]);
+            if (GenContents_quantity>1 && ignuin(0,1)) xmlprintf(out,", Dutch");
+            break;
+        case LOCATION:
+        case COUNTRY:
+            if (genunf(0,1)<0.75) GenContents_country=countries_USA;
+            else GenContents_country=ignuin(0,countries_len-1);
+            xmlprintf(out, "%s", countries[GenContents_country]);
+            break;
+        case PROVINCE:
+            if (GenContents_country==countries_USA)
+                xmlprintf(out,"%s", provinces[ignuin(0,provinces_len-1)]);
+            else
+                xmlprintf(out,"%s", lastnames[ignuin(0,lastnames_len-1)]);
+            break;
+        case EDUCATION:            
+            xmlprintf(out, "%s", GenContents_education[ignuin(0,3)]);
+            break;
+        case STATUS:
+        case HAPPINESS:
+            xmlprintf(out,"%d",ignuin(1,10));
+            break;
+        case HOMEPAGE:
+            xmlprintf(out,"http://www.%s/~%s", emails[GenContents_email],lastnames[GenContents_lstname]);
+            break;
+        case STREET:
+            r=ignuin(0,lastnames_len-1);
+            xmlprintf(out,"%d %s St",ignuin(1,100),lastnames[r]);
+            break;
+        case PHONE:
+            int country=ignuin(1,99);
+            int area=ignuin(10,999);
+            int number=ignuin(123456,98765432);
+            xmlprintf(out,"+%d (%d) %d",country,area,number);
+            break;
+        case CREDITCARD:
+            for(int i=0;i<4;i++) {
+                char *s = "";
+                if (i < 3) {
+                    s = " ";
+                }
+                xmlprintf(out,"%d%s",ignuin(1000,9999), s);
+            }
+            break;
+        case PAYMENT:
+            r=0;
+            for (int i=0;i<4;i++)
+                if (ignuin(0,1)) {
+                    char *x = "";
+                    if (r++) {
+                        x = ", ";
+                    }
+                    xmlprintf(out, "%s%s", x, GenContents_money[i % 4]);
+                }
+            break;
+        case SHIPPING:
+            r=0;
+            for (int i=0;i<4;i++)
+                if (ignuin(0,1)) {
+                    char *s = "";
+                    if (r++) s = ", ";
+                    xmlprintf(out,"%s%s",s,GenContents_shipping[i % 4]);
+                }
+            break;
+        case TIME:
+            int hrs=ignuin(0,23);
+            int min=ignuin(0,59);
+            int sec=ignuin(0,59);
+            xmlprintf(out,"%02d:%02d:%02d",hrs,min,sec);
+            break;
+        case AGE:
+            r=(int)gennor(30,15);
+            if (r < 18) r = 18;
+            xmlprintf(out,"%d", r);
+            break;
+        case ZIPCODE:
+            r=ignuin(3,4);
+            int cd = 10;
+            for (int j = 0; j < r; j++) {
+                cd*=10;
+            }
+            r=ignuin(r, 10*r - 1);
+            xmlprintf(out,"%d",r);
+            break;
+        case BUSINESS:
+        case PRIVACY:
+            xmlprintf(out,GenContents_yesno[ignuin(0,1)]);
+            break;
+        case XDATE:
+        case START:
+        case END:
+            int month=ignuin(1,12);
+            int day=ignuin(1,28);
+            int year=ignuin(1998,2001);
+            xmlprintf(out,"%02d/%02d/%4d",month,day,year);
+            break;
+        case CATNAME:
+        case ITEMNAME:
+            PrintSentence(ignuin(1,4));
+            break;
+        case NAME:
+            PrintName(&GenContents_lstname);
+            break;
+        case FROM:
+        case TO:
+            PrintName(&GenContents_lstname);
+            xmlprintf(out," ");
+            break;
+        case EMAIL:
+            GenContents_email=ignuin(0,emails_len-1);
+            xmlprintf(out,"mailto:%s@%s",lastnames[GenContents_lstname],emails[GenContents_email]);
+            break;
+        case GENDER:
+            r=ignuin(0,1);
+            if (r < 1) {
+                xmlprintf(out,"%s","male");
+            } else {
+                xmlprintf(out,"%s","female");
+            }
+            break;
+        case QUANTITY:
+            GenContents_quantity=1+(int)genexp(0.4);
+            xmlprintf(out,"%d",GenContents_quantity);
+            break;
+        case INCREASE:
+            double d=1.5 *(1+(int)genexp(10));
+            xmlprintf(out,"%.2f",d);
+            GenContents_increases+=d;
+        break;
+        case CURRENT:
+            xmlprintf(out,"%.2f",GenContents_initial+GenContents_increases);
+            break;
+        case INIT_PRICE:
+            GenContents_initial=genexp(100);
+            GenContents_increases=0;
+            xmlprintf(out,"%.2f",GenContents_initial);
+            break;
+        case AMOUNT:
+        case PRICE:
+            xmlprintf(out,"%.2f",genexp(100));
+            break;
+        case RESERVE:
+            xmlprintf(out,"%.2f",GenContents_initial*(1.2+genexp(2.5)));
+            break;
+        case TEXT:
+            PrintANY();
+            break;
+        default:
+            result=0;
+    }
+    if (result && (od->elm[0].id != 0)) {
         xmlprintf(out,"\n");
     }
     if (od->type&0x02)
@@ -1275,167 +1432,6 @@ char *GenContents_shipping[]={
                 "Will ship internationally",
                 "Buyer pays fixed shipping charges",
                 "See description for charges"};
-
-pub int GenContents(FILE *xmlout, int type) {
-    int r;
-    int result = 1;
-    switch(type) {
-        case CITY:
-            xmlprintf(xmlout, "%s", cities[ignuin(0,cities_len-1)]);
-            break;
-        case TYPE:
-            xmlprintf(xmlout, "%s", GenContents_auction_type[ignuin(0,1)]);
-            if (GenContents_quantity>1 && ignuin(0,1)) xmlprintf(xmlout,", Dutch");
-            break;
-        case LOCATION:
-        case COUNTRY:
-            if (genunf(0,1)<0.75) GenContents_country=countries_USA;
-            else GenContents_country=ignuin(0,countries_len-1);
-            xmlprintf(xmlout, "%s", countries[GenContents_country]);
-            break;
-        case PROVINCE:
-            if (GenContents_country==countries_USA)
-                xmlprintf(xmlout,"%s", provinces[ignuin(0,provinces_len-1)]);
-            else
-                xmlprintf(xmlout,"%s", lastnames[ignuin(0,lastnames_len-1)]);
-            break;
-        case EDUCATION:            
-            xmlprintf(xmlout, "%s", GenContents_education[ignuin(0,3)]);
-            break;
-        case STATUS:
-        case HAPPINESS:
-            xmlprintf(xmlout,"%d",ignuin(1,10));
-            break;
-        case HOMEPAGE:
-            xmlprintf(xmlout,"http://www.%s/~%s", emails[GenContents_email],lastnames[GenContents_lstname]);
-            break;
-        case STREET:
-            r=ignuin(0,lastnames_len-1);
-            xmlprintf(xmlout,"%d %s St",ignuin(1,100),lastnames[r]);
-            break;
-        case PHONE:
-            int country=ignuin(1,99);
-            int area=ignuin(10,999);
-            int number=ignuin(123456,98765432);
-            xmlprintf(xmlout,"+%d (%d) %d",country,area,number);
-            break;
-        case CREDITCARD:
-            for(int i=0;i<4;i++) {
-                char *s = "";
-                if (i < 3) {
-                    s = " ";
-                }
-                xmlprintf(xmlout,"%d%s",ignuin(1000,9999), s);
-            }
-            break;
-        case PAYMENT:
-            r=0;
-            for (int i=0;i<4;i++)
-                if (ignuin(0,1)) {
-                    char *x = "";
-                    if (r++) {
-                        x = ", ";
-                    }
-                    xmlprintf(xmlout, "%s%s", x, GenContents_money[i % 4]);
-                }
-            break;
-        case SHIPPING:
-            r=0;
-            for (int i=0;i<4;i++)
-                if (ignuin(0,1)) {
-                    char *s = "";
-                    if (r++) s = ", ";
-                    xmlprintf(xmlout,"%s%s",s,GenContents_shipping[i % 4]);
-                }
-            break;
-        case TIME:
-            int hrs=ignuin(0,23);
-            int min=ignuin(0,59);
-            int sec=ignuin(0,59);
-            xmlprintf(xmlout,"%02d:%02d:%02d",hrs,min,sec);
-            break;
-        case AGE:
-            r=(int)gennor(30,15);
-            if (r < 18) r = 18;
-            xmlprintf(xmlout,"%d", r);
-            break;
-        case ZIPCODE:
-            r=ignuin(3,4);
-            int cd = 10;
-            for (int j = 0; j < r; j++) {
-                cd*=10;
-            }
-            r=ignuin(r, 10*r - 1);
-            xmlprintf(xmlout,"%d",r);
-            break;
-        case BUSINESS:
-        case PRIVACY:
-            xmlprintf(xmlout,GenContents_yesno[ignuin(0,1)]);
-            break;
-        case XDATE:
-        case START:
-        case END:
-            int month=ignuin(1,12);
-            int day=ignuin(1,28);
-            int year=ignuin(1998,2001);
-            xmlprintf(xmlout,"%02d/%02d/%4d",month,day,year);
-            break;
-        case CATNAME:
-        case ITEMNAME:
-            PrintSentence(ignuin(1,4));
-            break;
-        case NAME:
-            PrintName(&GenContents_lstname);
-            break;
-        case FROM:
-        case TO:
-            PrintName(&GenContents_lstname);
-            xmlprintf(xmlout," ");
-            break;
-        case EMAIL:
-            GenContents_email=ignuin(0,emails_len-1);
-            xmlprintf(xmlout,"mailto:%s@%s",lastnames[GenContents_lstname],emails[GenContents_email]);
-            break;
-        case GENDER:
-            r=ignuin(0,1);
-            if (r < 1) {
-                xmlprintf(xmlout,"%s","male");
-            } else {
-                xmlprintf(xmlout,"%s","female");
-            }
-            break;
-        case QUANTITY:
-            GenContents_quantity=1+(int)genexp(0.4);
-            xmlprintf(xmlout,"%d",GenContents_quantity);
-            break;
-        case INCREASE:
-            double d=1.5 *(1+(int)genexp(10));
-            xmlprintf(xmlout,"%.2f",d);
-            GenContents_increases+=d;
-        break;
-        case CURRENT:
-            xmlprintf(xmlout,"%.2f",GenContents_initial+GenContents_increases);
-            break;
-        case INIT_PRICE:
-            GenContents_initial=genexp(100);
-            GenContents_increases=0;
-            xmlprintf(xmlout,"%.2f",GenContents_initial);
-            break;
-        case AMOUNT:
-        case PRICE:
-            xmlprintf(xmlout,"%.2f",genexp(100));
-            break;
-        case RESERVE:
-            xmlprintf(xmlout,"%.2f",GenContents_initial*(1.2+genexp(2.5)));
-            break;
-        case TEXT:
-            PrintANY();
-            break;
-        default:
-            result=0;
-        }
-    return result;
-}
 
 void PrintName(int *lastout)
 {
