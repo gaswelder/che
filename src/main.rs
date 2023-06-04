61ce14d9d582a57ba03b26e28b5571983d8f8167
gasweld <gasweld@gmail.com>
2023-06-04 03:02:11 +0300

    Split the test command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod build;
 mod checkers;
 mod format;
 mod lexer;
+mod main_test;
 mod nodes;
 mod parser;
 #[cfg(test)]
@@ -11,9 +12,10 @@ mod translator;
 #[cfg(test)]
 use pretty_assertions::assert_eq;
 use std::env;
+#[cfg(test)]
 use std::fs;
 use std::path::Path;
-use std::process::{exit, Command};
+use std::process::exit;
 use std::str;
 use std::string::String;
 
@@ -58,7 +60,7 @@ fn main() {
         exit(exports(&args[2..]));
     }
     if args[1] == "test" {
-        match run_tests(&args[2..]) {
+        match main_test::run_tests(&args[2..]) {
             Err(s) => {
                 eprintln!("{}", s);
                 exit(1);
@@ -74,57 +76,6 @@ fn main() {
 fn usage() {
     eprintln!("usage: che build | deptree | exports | test");
     exit(1);
-}
-
-fn find_tests(dirpath: &String) -> Result<Vec<String>, String> {
-    let mut paths: Vec<String> = Vec::new();
-    for entry_result in fs::read_dir(dirpath).unwrap() {
-        let result = entry_result.unwrap();
-        let path = result.path();
-        if path.to_str().unwrap().ends_with(".test.c") {
-            paths.push(String::from(path.to_str().unwrap()));
-        } else if path.is_dir() {
-            paths.append(&mut find_tests(&String::from(path.to_str().unwrap())).unwrap())
-        }
-    }
-    return Ok(paths);
-}
-
-fn run_tests(args: &[String]) -> Result<(), String> {
-    let mut fails = 0;
-    let mut total = 0;
-    for arg in args {
-        let tests = find_tests(arg)?;
-        for path in tests {
-            build::build_prog(&path, &String::from("test.out")).unwrap();
-            let output = Command::new("./test.out").output().unwrap();
-            let errstr = str::from_utf8(&output.stderr).unwrap();
-            let outstr = str::from_utf8(&output.stdout).unwrap().trim_end();
-            if output.status.success() {
-                if outstr != "" {
-                    println!("OK {}: {}", path, outstr);
-                } else {
-                    println!("OK {}", path);
-                }
-            } else {
-                fails += 1;
-                println!("FAIL {}", path);
-                println!("stdout:\n{}", outstr);
-            }
-            if errstr != "" {
-                println!("stderr:\n{}", errstr);
-            }
-            total += 1
-        }
-        println!("{} tests", total)
-    }
-    if fails > 1 {
-        return Err(format!("{} tests failed", fails));
-    }
-    if fails == 1 {
-        return Err(format!("1 test failed"));
-    }
-    return Ok(());
 }
 
 fn basename(path: &String) -> String {
