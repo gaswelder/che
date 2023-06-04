1d429a69925a099963bb52bfda458886afd594a7
gasweld <gasweld@gmail.com>
2023-06-04 03:07:23 +0300

    Group main's tests

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,11 @@ mod parser;
 #[cfg(test)]
 mod rename;
 mod translator;
-#[cfg(test)]
-use pretty_assertions::assert_eq;
 use std::env;
-#[cfg(test)]
-use std::fs;
 use std::path::Path;
 use std::process::exit;
 use std::str;
 use std::string::String;
-
-#[test]
-fn test_rename() {
-    let mut m = parser::get_module(&"json".to_string(), &String::from(".")).unwrap();
-    let exports = checkers::get_exports(&m);
-    let mut names: Vec<String> = Vec::new();
-    for e in exports.consts {
-        names.push(e.id);
-    }
-    for f in exports.fns {
-        names.push(f.form.name);
-    }
-    for t in exports.types {
-        names.push(t.form.alias);
-    }
-    println!("{:?}", names);
-    rename::rename_mod(&mut m, &"kek".to_string(), &names);
-    let c = translator::translate(&m);
-    fs::write("out.c", format::format_compat_module(&c)).unwrap();
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -80,33 +56,6 @@ fn usage() {
 
 fn basename(path: &String) -> String {
     return String::from(Path::new(path).file_name().unwrap().to_str().unwrap());
-}
-
-#[test]
-fn snapshots() {
-    for entry in fs::read_dir("test/snapshots").unwrap() {
-        let tmp = entry.unwrap().path();
-        let path = tmp.to_str().unwrap();
-        if path.ends_with(".out.c") {
-            continue;
-        }
-        println!("{}", path);
-
-        // get the output
-        let p = &path.to_string();
-        let m = parser::get_module(&basename(p), &p).unwrap();
-        let mods = build::resolve_deps(&m);
-        let c_mods: Vec<nodes::CompatModule> =
-            mods.iter().map(|m| translator::translate(&m)).collect();
-        let result = format::format_compat_module(&c_mods[0]);
-
-        let snapshot_path = path.replace(".c", ".out.c");
-        if env::var("UPDATE_SNAPSHOT").is_ok() {
-            fs::write(snapshot_path, result).unwrap();
-        } else {
-            assert_eq!(result, fs::read_to_string(snapshot_path).unwrap());
-        }
-    }
 }
 
 fn build(argv: &[String]) -> Result<(), String> {
@@ -251,4 +200,70 @@ fn exports(argv: &[String]) -> i32 {
         }
     }
     return 0;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::build;
+    use crate::checkers;
+    use crate::format;
+    use crate::nodes;
+    use crate::parser;
+    use crate::rename;
+    use crate::translator;
+    use pretty_assertions::assert_eq;
+    use std::env;
+    use std::fs;
+    use std::path::Path;
+
+    fn basename(path: &String) -> String {
+        return String::from(Path::new(path).file_name().unwrap().to_str().unwrap());
+    }
+
+    #[test]
+    fn test_rename() {
+        let mut m = parser::get_module(&"json".to_string(), &String::from(".")).unwrap();
+        let exports = checkers::get_exports(&m);
+        let mut names: Vec<String> = Vec::new();
+        for e in exports.consts {
+            names.push(e.id);
+        }
+        for f in exports.fns {
+            names.push(f.form.name);
+        }
+        for t in exports.types {
+            names.push(t.form.alias);
+        }
+        println!("{:?}", names);
+        rename::rename_mod(&mut m, &"kek".to_string(), &names);
+        let c = translator::translate(&m);
+        fs::write("out.c", format::format_compat_module(&c)).unwrap();
+    }
+
+    #[test]
+    fn snapshots() {
+        for entry in fs::read_dir("test/snapshots").unwrap() {
+            let tmp = entry.unwrap().path();
+            let path = tmp.to_str().unwrap();
+            if path.ends_with(".out.c") {
+                continue;
+            }
+            println!("{}", path);
+
+            // get the output
+            let p = &path.to_string();
+            let m = parser::get_module(&basename(p), &p).unwrap();
+            let mods = build::resolve_deps(&m);
+            let c_mods: Vec<nodes::CompatModule> =
+                mods.iter().map(|m| translator::translate(&m)).collect();
+            let result = format::format_compat_module(&c_mods[0]);
+
+            let snapshot_path = path.replace(".c", ".out.c");
+            if env::var("UPDATE_SNAPSHOT").is_ok() {
+                fs::write(snapshot_path, result).unwrap();
+            } else {
+                assert_eq!(result, fs::read_to_string(snapshot_path).unwrap());
+            }
+        }
+    }
 }
