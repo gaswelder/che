9ea499b7af3d1f18013e3907e6cad484eef3d37b
gasweld <gasweld@gmail.com>
2023-06-10 03:32:14 +0300

    Fix the test scoped build

--- test/scoped/lib.c
+++ test/scoped/lib.c
@@ -1,5 +1,6 @@
 pub typedef { int a; } foo_t;
 pub void f() {
     foo_t x = {};
+    printf("x.a = %d\n", x.a);
     puts("OK");
 }
--- test/scoped/main.c
+++ test/scoped/main.c
@@ -2,6 +2,7 @@
 
 int main() {
     lib.foo_t f = {};
+    printf("size = %zu\n", sizeof(f));
     lib.f();
     return 0;
 }
