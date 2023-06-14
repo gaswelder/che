79599e735def4344149c096e2b194f31912a0b25
gasweld <gasweld@gmail.com>
2023-06-14 12:04:28 +0300

    Add fs.realpath

---
+++ lib/fs.c
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <stdlib.h>
+
+pub bool *realpath(const char *path, char *buf, size_t n) {
+    char tmp[PATH_MAX] = {};
+    if (!OS.realpath(path, buf)) {
+        return false;
+    }
+    if (strlen(tmp) > n-1) {
+        // buf is too small.
+        return false;
+    }
+    strcpy(buf, tmp);
+    return true;
+}
