9c31218d37b51c101e2e1dda9e1015a45639eda8
gasweld <gasweld@gmail.com>
2023-06-19 22:19:57 +0300

    Move a mem test to lib

---
+++ lib/mem.test.c
@@ -0,0 +1,30 @@
+#import mem
+#import test
+
+int main() {
+	mem.mem_t *m = mem.memopen();
+	if (!m) {
+		panic("memopen failed");
+	}
+
+	// Fill the buffer with some data.
+	const char *sample = "012345678";
+	size_t len = strlen(sample);
+	size_t r = mem.memwrite(m, sample, len);
+	if (r < len) {
+		panic("memwrite failed");
+	}
+
+	// Rewind the buffer and check the contents.
+	const char *p = sample;
+	mem.memrewind(m);
+	while (true) {
+		int c = mem.memgetc(m);
+		if (c == EOF) {
+			break;
+		}
+		test.truth("memgetc == *p", c == *p++);
+	}
+	mem.memclose(m);
+	return 0;
+}
