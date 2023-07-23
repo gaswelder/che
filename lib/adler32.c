90afadad0d03faedb9776c20d6d405107ae583cd
gasweld <gasweld@gmail.com>
2023-07-23 15:39:36 +0300

    Extract adler32

--- lib/adler32.c
+++ lib/adler32.c
@@ -27,18 +27,10 @@
 #define NMAX 5552
 /* NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 */
 
-#define DO1(buf,i)  {adler += (buf)[i]; sum2 += adler;}
-#define DO2(buf,i)  DO1(buf,i); DO1(buf,i+1);
-#define DO4(buf,i)  DO2(buf,i); DO2(buf,i+2);
-#define DO8(buf,i)  DO4(buf,i); DO4(buf,i+4);
-
-#define MOD(a) a %= BASE
-#define MOD28(a) a %= BASE
-
 /*
      Update a running Adler-32 checksum with the bytes buf[0..len-1] and
    return the updated checksum. An Adler-32 value is in the range of a 32-bit
-   unsigned integer. If buf is Z_NULL, this function returns the required
+   uint32_t integer. If buf is Z_NULL, this function returns the required
    initial value for the checksum.
 
      An Adler-32 checksum is almost as reliable as a CRC-32 but can be computed
@@ -70,7 +62,7 @@ pub uint32_t adler32(uint32_t adler, const uint8_t *buf, size_t len) {
     }
 
     /* initial Adler-32 value (deferred check for len == 1 speed) */
-    if (buf == Z_NULL)
+    if (buf == NULL)
         return 1L;
 
     /* in case short lengths are provided, keep it somewhat fast */
@@ -79,38 +71,49 @@ pub uint32_t adler32(uint32_t adler, const uint8_t *buf, size_t len) {
             adler += *buf++;
             sum2 += adler;
         }
-        if (adler >= BASE)
+        if (adler >= BASE) {
             adler -= BASE;
-        MOD28(sum2);            /* only added so many BASE's */
+        }
+        sum2 %= BASE;            /* only added so many BASE's */
         return adler | (sum2 << 16);
     }
 
     /* do length NMAX blocks -- requires just one modulo operation */
-    unsigned n;
+    uint32_t n;
     while (len >= NMAX) {
         len -= NMAX;
         n = NMAX / 16;          /* NMAX is divisible by 16 */
-        do {
-            DO8(buf,0); DO8(buf,8);          /* 16 sums unrolled */
+        while (true) {
+            for (int roll = 0; roll < 16; roll++) {
+                adler += buf[roll];
+                sum2 += adler;
+            }
             buf += 16;
-        } while (--n);
-        MOD(adler);
-        MOD(sum2);
+            n--;
+            if (!n) {
+                break;
+            }
+        }
+        adler %= BASE;
+        sum2 %= BASE;
     }
 
     /* do remaining bytes (less than NMAX, still just one modulo) */
     if (len) {                  /* avoid modulos if none remaining */
         while (len >= 16) {
             len -= 16;
-            DO8(buf,0); DO8(buf,8);
+            for (int roll = 0; roll < 16; roll++) {
+                adler += buf[roll];
+                sum2 += adler;
+            }
             buf += 16;
         }
         while (len--) {
             adler += *buf++;
             sum2 += adler;
         }
-        MOD(adler);
-        MOD(sum2);
+        adler %= BASE;
+        sum2 %= BASE;
     }
 
     /* return recombined sums */
@@ -124,36 +127,26 @@ pub uint32_t adler32(uint32_t adler, const uint8_t *buf, size_t len) {
    that the z_off_t type (like off_t) is a signed integer.  If len2 is
    negative, the result has no meaning or utility.
 */
-pub uint32_t adler32_combine(uint32_t adler1, adler2, z_off_t len2) {
-    return adler32_combine_(adler1, adler2, len2);
-}
-
-pub uint32_t adler32_combine64(uint32_t adler1, adler2, z_off64_t len2) {
-    return adler32_combine_(adler1, adler2, len2);
-}
-
-
-uint32_t adler32_combine_(uint32_t adler1, adler2, z_off64_t len2) {
-    unsigned long sum1;
-    unsigned long sum2;
-    unsigned rem;
+pub uint32_t adler32_combine(uint32_t adler1, adler2, int64_t len2) {
+    uint32_t sum1;
+    uint32_t sum2;
+    uint32_t rem;
 
     /* for negative len, return invalid adler32 as a clue for debugging */
     if (len2 < 0)
         return 0xffffffffUL;
 
     /* the derivation of this formula is left as an exercise for the reader */
-    len32 %= BASE; /* assumes len2 >= 0 */
-    rem = (unsigned)len2;
+    len2 %= BASE; /* assumes len2 >= 0 */
+    rem = (uint32_t)len2;
     sum1 = adler1 & 0xffff;
     sum2 = rem * sum1;
-    MOD(sum2);
+    sum2 %= BASE;
     sum1 += (adler2 & 0xffff) + BASE - 1;
     sum2 += ((adler1 >> 16) & 0xffff) + ((adler2 >> 16) & 0xffff) + BASE - rem;
     if (sum1 >= BASE) sum1 -= BASE;
     if (sum1 >= BASE) sum1 -= BASE;
-    if (sum2 >= ((unsigned long)BASE << 1)) sum2 -= ((unsigned long)BASE << 1);
+    if (sum2 >= ((uint32_t)BASE << 1)) sum2 -= ((uint32_t)BASE << 1);
     if (sum2 >= BASE) sum2 -= BASE;
     return sum1 | (sum2 << 16);
 }
-
