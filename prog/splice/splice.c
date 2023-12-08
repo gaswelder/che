2ea189514fa21c60cec8769e95e1e321e3b316cb
gasweld <gasweld@gmail.com>
2023-12-08 13:17:07 +0200

    Show help in splice

--- prog/splice/splice.c
+++ prog/splice/splice.c
@@ -6,8 +6,15 @@ int main(int argc, char *argv[]) {
     size_t size = 2;
     size_t cols = 0;
 
+    bool hflag = false;
     opt.opt_size("n", "number of columns", &size);
+    opt.opt_bool("h", "show help", &hflag);
     opt.opt_parse(argc, argv);
+
+    if (hflag) {
+        opt.opt_usage();
+        return 0;
+    }
 
     while (fgets(buf, 4096, stdin)) {
         strings.rtrim(buf, "\n");
