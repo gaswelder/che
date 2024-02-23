0be9fa39a26b815bffbf316c1f175b66664cec57
gasweld <gasweld@gmail.com>
2024-02-23 21:06:59 +0200

    dbg: support multiple tags

--- lib/dbg.c
+++ lib/dbg.c
@@ -11,6 +11,36 @@ pub void m(const char *tag, *format, ...) {
 }
 
 bool dbg_enabled(const char *tag) {
+	if (!parsed) {
+		parse();
+		parsed = true;
+	}
+	for (size_t i = 0; i < n; i++) {
+		if (strcmp(list[i], tag) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool parsed = false;
+const char *list[10] = {};
+size_t n = 0;
+
+void parse() {
 	const char *val = misc.getenv("CHE_DEBUG");
-	return val && strcmp(val, tag) == 0;
+	if (!val) return;
+
+	char *copy = calloc(strlen(val)+1, 1);
+	strcpy(copy, val);
+	char *p = copy;
+	list[n++] = p;
+	while (*p) {
+		if (*p == ',') {
+			*p = '\0';
+			if (n == 9) break;
+			list[n++] = p+1;
+		}
+		p++;
+	}
 }
