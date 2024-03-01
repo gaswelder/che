8f7b8345a493af0b410c332550b7d9ad531ecfc7
gasweld <gasweld@gmail.com>
2024-03-01 22:24:08 +0200

    reader: convert to an interface

--- lib/reader.c
+++ lib/reader.c
@@ -1,37 +1,70 @@
 pub typedef {
+	int type;
+	void *data;
+} t;
+
+typedef {
 	const char *s;
 	size_t pos;
 	size_t len;
-} t;
+} str_t;
+
+enum {
+	STR = 1,
+};
 
 pub t *string(const char *s) {
 	t *r = calloc(1, sizeof(t));
 	if (!r) {
 		return NULL;
 	}
-	r->s = s;
-	r->len = strlen(s);
+	r->type = STR;
+	str_t *data = calloc(1, sizeof(str_t));
+	if (!data) {
+		OS.free(r);
+		return NULL;
+	}
+	data->s = s;
+	data->len = strlen(s);
+	r->data = data;
 	return r;
 }
 
 pub void free(t *reader) {
+	OS.free(reader->data);
 	OS.free(reader);
 }
 
-pub int peek(t *b) {
-	if (b->pos >= b->len) {
-		return EOF;
+pub int peek(t *reader) {
+	switch (reader->type) {
+		case STR: {
+			str_t *data = reader->data;
+			if (data->pos >= data->len) return EOF;
+			return data->s[data->pos];
+		}
 	}
-	return b->s[b->pos];
+	panic("unknown type: %d", reader->type);
 }
 
-pub bool more(t *b) {
-	return b->pos < b->len;
+pub bool more(t *reader) {
+	switch (reader->type) {
+		case STR: {
+			str_t *data = reader->data;
+			return data->pos < data->len;
+		}
+	}
+	panic("unknown type: %d", reader->type);
 }
 
-pub int get(t *b) {
-	if (b->pos >= b->len) {
-		return EOF;
+pub int get(t *reader) {
+	switch (reader->type) {
+		case STR: {
+			str_t *data = reader->data;
+			if (data->pos >= data->len) {
+				return EOF;
+			}
+			return data->s[data->pos++];
+		}
 	}
-	return b->s[b->pos++];
+	panic("unknown type: %d", reader->type);
 }
