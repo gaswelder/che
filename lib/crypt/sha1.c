c624e359956c2c60f17ba6262d9b63857f80dfbb
gasweld <gasweld@gmail.com>
2024-08-28 23:52:27 +0300

    sha1: use static buffers

--- lib/crypt/sha1.c
+++ lib/crypt/sha1.c
@@ -2,74 +2,23 @@
  * http://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf
  */
 
-#import mem
-
 pub typedef {
-	mem.mem_t *z;
+	bool init;
 	size_t pos;
 	bool finished;
 	uint32_t sum[5];
+
+	uint32_t _block[16];
+	size_t _words;
+	size_t _bytes;
+	uint32_t _word;
+
+	uint64_t length; // current message length in bits.
+
 } digest_t;
 
-void begin(digest_t *hash) {
-	hash->z = mem.memopen();
-	if (!hash->z) panic("memopen failed");
-	/*
-	 * Set the initial sum.
-	 */
-	hash->sum[0] = 0x67452301;
-	hash->sum[1] = 0xefcdab89;
-	hash->sum[2] = 0x98badcfe;
-	hash->sum[3] = 0x10325476;
-	hash->sum[4] = 0xc3d2e1f0;
-}
-
-pub bool add(digest_t *hash, char byte) {
-	/*
-	 * Make sure add is not called after the end.
-	 */
-	if (hash->finished) return false;
-	/*
-	 * On the first add allocate memory for the data.
-	 */
-	if (!hash->z) {
-		begin(hash);
-	}
-	/*
-	 * Write to the buffer.
-	 */
-	mem.memputc(byte, hash->z);
-	hash->pos++;
-	return true;
-}
-
-pub bool end(digest_t *hash) {
-	if (hash->finished) return false;
-	hash->finished = true;
-
-	if (!hash->z) {
-		hash->z = mem.memopen();
-		if (!hash->z) panic("memopen failed");
-	}
-
-	uint32_t *sum = hash->sum;
-
-	mem.mem_t *data = hash->z;
-	mem.memrewind(data);
-	src_t s = {};
-	src_init(&s, data);
-
-	uint32_t block[16] = {};
-	while (s.more) {
-		for (int i = 0; i < 16; i++) {
-			block[i] = next_word(&s);
-		}
-		sha1_feed(block, sum);
-	}
-
-	mem.memclose(data);
-
-	return true;
+pub void reset(digest_t *hash) {
+	memset(hash, 0, sizeof(digest_t));
 }
 
 const char HEX[] = "0123456789abcdef";
@@ -108,120 +57,81 @@ pub void as_bytes(digest_t *hash, uint8_t *buf) {
 	}
 }
 
-/*
- * The internal "machinery" processes a stream of 64-byte blocks.
- * The message is followed by an EOF byte, followed by padding and length:
- *
- * | message | bits "10000000" | padding | length |
- *
- * 'length' is a 64-bit encoding of the message length.
- * The padding is so many zero bytes that the end of stream happens is at
- * a multiple of 64 bytes.
- */
-typedef {
-	mem.mem_t *stream; // data stream
+pub bool add(digest_t *hash, char byte) {
+	// Make sure add is not called after the end.
+	if (hash->finished) return false;
 
-	uint64_t length; // current message length in bits
-	bool more_data; // set to false after the end of data
-	int zeros; // how many zeros left to put out
+	// On the first add set the initial sum.
+	if (!hash->init) {
+		hash->init = true;
+		hash->sum[0] = 0x67452301;
+		hash->sum[1] = 0xefcdab89;
+		hash->sum[2] = 0x98badcfe;
+		hash->sum[3] = 0x10325476;
+		hash->sum[4] = 0xc3d2e1f0;
+	}
 
-	uint8_t lenbuf[8]; // buffer with the message length representation
-	int lenpos; // current position in lenbuf
-
-	bool more;
-} src_t;
-
-void src_init(src_t *s, mem.mem_t *data)
-{
-	s->stream = data;
-	s->length = 0;
-	s->more_data = true;
-	s->more = true;
+	hash->pos++;
+	hash->length += 8;
+	push_byte(hash, byte);
+	return true;
 }
 
-uint32_t next_word(src_t *s)
-{
-	uint32_t word = 0;
-	for(int i = 0; i < 4; i++) {
-		uint8_t b = next_byte(s);
-		word *= 256;
-		word += b;
-	}
-	return word;
-}
+pub bool end(digest_t *hash) {
+	if (hash->finished) return false;
+	hash->finished = true;
 
-uint8_t next_byte(src_t *s)
-{
-	if(s->more_data) {
-		int c = mem.memgetc(s->stream);
-		if(c != EOF) {
-			s->length += 8;
-			return (uint8_t) c;
-		}
-		s->more_data = false;
+	// The internal "machinery" processes a stream of 64-byte blocks.
+	// The message is followed by an EOF byte, followed by padding and length:
+	//
+	//      message | bits "10000000" | padding | length |
+	//
+	// 'length' is a 64-bit encoding of the message length.
+	// The padding is so many zero bytes that the end of stream happens is at
+	// a multiple of 64 bytes.
 
-		/*
-		 * Calculate padding parameters now that we know the
-		 * actual message length.
-		 */
-		init_padding(s);
+	// Bits "10000000" from the spec are simply number 128.
+	push_byte(hash, 128);
 
-		/*
-		 * The spec says to add bit '1' to the end of the actual
-		 * data and then put zeros until a specific position is
-		 * reached. We can just add the whole byte with first bit
-		 * set to '1' because we will not reach that special
-		 * position for at least 7 more bits. And this byte is
-		 * actually value 128 (because bytes are treated as big-endian).
-		 */
-		return 128;
-	}
-
-	if(s->zeros > 0) {
-		s->zeros--;
-		return 0;
-	}
-
-	assert((size_t) s->lenpos < sizeof(s->lenbuf));
-	uint8_t b = s->lenbuf[s->lenpos++];
-	/*
-	 * Put a flag after the stream is finished.
-	 */
-	if(s->lenpos == sizeof(s->lenbuf)) {
-		s->more = false;
-	}
-	return b;
-}
-
-void init_padding(src_t *s) {
-	/*
-	 * Now we know 'b' (data length in bits), so we can create the
-	 * length mark and calculate how many zero bytes need to be added.
-	 */
-
-	int i = 0;
-	uint8_t b = 0;
-	uint8_t *pos = (uint8_t *) &(s->lenbuf);
-	for(i = 0; i < 8; i++) {
-		b = (s->length >> (64-8 - 8*i)) & 0xFF;
-		*pos = b;
-		pos++;
-	}
-	s->lenpos = 0;
-
-	/*
-	 * (length + 1 + zeros + 8) % 64 == 0
-	 * where '1' is for the 'eof' byte and '8' is for the length marker.
-	 */
-	int l = s->length/8;
+	// (length + 1 + zeros + 8) % 64 == 0
+	// where '1' is for the 'eof' byte and '8' is for the length marker.
+	int l = hash->length / 8;
 	int n = (9 + l) / 64;
-	if((9 + l) % 64 != 0) {
-		n++;
+	if ((9 + l) % 64 != 0) n++;
+	size_t zeros = 64 * n - 9 - l;
+	while (zeros > 0) {
+		zeros--;
+		push_byte(hash, 0);
 	}
-	s->zeros = 64 * n - 9 - l;
+
+	// Append the message length.
+	for (int i = 0; i < 8; i++) {
+		uint8_t b = (hash->length >> (64-8 - 8*i)) & 0xFF;
+		push_byte(hash, b);
+	}
+	return true;
 }
 
+void push_byte(digest_t *hash, uint8_t b) {
+	uint32_t *sum = hash->sum;
 
+	// Collect a full word.
+	// When a full word is collected, push it to the block.
+	hash->_word *= 256;
+	hash->_word += b;
+	hash->_bytes++;
+	if (hash->_bytes == 4) {
+		hash->_bytes = 0;
+
+		// Collect a full block.
+		// When a full block is collected, feed it into the hash.
+		hash->_block[hash->_words++] = hash->_word;
+		if (hash->_words == 16) {
+			hash->_words = 0;
+			sha1_feed(hash->_block, sum);
+		}
+	}
+}
 
 /**
  * Feeds the next data block into the current sum.
