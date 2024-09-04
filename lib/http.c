774b2c3827c013437d358979f79f542cd51e5e13
gasweld <gasweld@gmail.com>
2024-09-04 22:43:10 +0300

    http: add urlencode

--- lib/http.c
+++ lib/http.c
@@ -343,3 +343,23 @@ pub bool parse_response(char *data, response_t *r) {
 
     return true;
 }
+
+const char HEX[] = "0123456789ABCDEF";
+
+/**
+ * Puts url-encoded representation of buf into s.
+ */
+pub void url_encode(char *s, char *buf, size_t bufsize) {
+	char *p = s;
+	for (size_t i = 0 ; i < bufsize; i++) {
+		char c = buf[i];
+		if (isalpha(c) || isdigit(c)) {
+			*p++ = c;
+			continue;
+		}
+		*p++ = '%';
+		*p++ = HEX[(c >> 4) & 0x0F];
+		*p++ = HEX[c & 0x0F];
+	}
+	*p++ = '\0';
+}
