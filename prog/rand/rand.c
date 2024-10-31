0a6eee10d6ae1da1d290eefe6b689f19e82b7384
gasweld <gasweld@gmail.com>
2024-10-31 02:31:22 +0200

    rand: allow combining charsets

--- prog/rand/rand.c
+++ prog/rand/rand.c
@@ -1,33 +1,47 @@
 #import rnd
 #import opt
 
+const char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const char *nums = "0123456789";
+const char *special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
 int main(int argc, char **argv) {
 	size_t n = 1;
 	size_t len = 16;
 	size_t seed = (size_t) time(NULL);
 	char *a = "an";
+	opt.nargs(0, "");
 	opt.size("n", "number of values to generate", &n);
 	opt.size("l", "string length", &len);
 	opt.size("s", "seed value", &seed);
-	opt.str("a", "alphabet (n is numberic, an is alphanumeric)", &a);
+	opt.str("a", "alphabet (s is special chars, n is numbers, a is alphabetic)", &a);
 	opt.parse(argc, argv);
 
-	const char *alpha;
-	if (!strcmp(a, "an")) {
-		alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-	} else if (!strcmp(a, "n")) {
-		alpha = "0123456789";
-	} else {
-		fprintf(stderr, "unknown alphabet name: %s\n", a);
-		return 1;
+	bool _a = false;
+	bool _n = false;
+	bool _s = false;
+	for (char *p = a; *p != '\0'; p++) {
+		switch (*p) {
+			case 'a': { _a = true; }
+			case 'n': { _n = true; }
+			case 's': { _s = true; }
+			default: {
+				fprintf(stderr, "unknown chars selector: %c\n", *p);
+				return 1;
+			}
+		}
 	}
-	size_t alphasize = strlen(alpha);
 
+	char set[200] = {};
+	if (_a) strcat(set, alpha);
+	if (_n) strcat(set, nums);
+	if (_s) strcat(set, special);
+
+	size_t alphasize = strlen(set);
 	rnd.seed((uint64_t) seed);
-
 	while (n-- > 0) {
 		for (size_t i = 0; i < len; i++) {
-			putchar(alpha[rnd.u32() % alphasize]);
+			putchar(set[rnd.u32() % alphasize]);
 		}
 		putchar('\n');
 	}
