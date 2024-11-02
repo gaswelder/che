6388d6ecca485633d81865e53cab35efc6cae989
gasweld <gasweld@gmail.com>
2024-11-02 19:25:58 +0200

    writer: add file writer

--- lib/writer.c
+++ lib/writer.c
@@ -1,8 +1,18 @@
+enum {
+	T_STATIC,
+	T_FILE,
+};
 
 pub typedef {
+	int type;
+
+	// T_STATIC:
 	char *data;
 	size_t pos;
 	size_t size;
+
+	// T_FILE:
+	FILE *f;
 } t;
 
 // Returns a writer to a static buffer.
@@ -10,8 +20,18 @@ pub typedef {
 pub t *static_buffer(char *data, size_t size) {
 	t *w = calloc(1, sizeof(t));
 	if (!w) panic("calloc failed");
+	w->type = T_STATIC;
 	w->data = data;
 	w->size = size;
+	return w;
+}
+
+// Returns a writer to a file.
+pub t *file(FILE *f) {
+	t *w = calloc(1, sizeof(t));
+	if (!w) panic("calloc failed");
+	w->type = T_FILE;
+	w->f = f;
 	return w;
 }
 
@@ -22,6 +42,12 @@ pub void free(t *w) {
 // Writes n bytes from data to w.
 // Returns the number of bytes written.
 pub int write(t *w, const char *data, size_t n) {
+	if (w->type == T_STATIC) return st_write(w, data, n);
+	size_t r = fwrite(data, 1, n, w->f);
+	return (int) r;
+}
+
+int st_write(t *w, const char *data, size_t n) {
 	if (w->pos >= w->size) {
 		return EOF;
 	}
