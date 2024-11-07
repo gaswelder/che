66bc1e3f37e686baed5768ed27fc08781d57f4ec
gasweld <gasweld@gmail.com>
2024-11-07 20:16:40 +0200

    writer: convert to interface

--- lib/writer.c
+++ lib/writer.c
@@ -1,20 +1,30 @@
-enum {
-	T_STATIC,
-	T_FILE,
-};
+
+pub typedef int writefunc_t(void *, const char *, size_t); // ctx, data, len
+pub typedef void freefunc_t(void *);
 
 pub typedef {
-	int type;
 	size_t nwritten;
-
-	// T_STATIC:
-	char *data;
-	size_t pos;
-	size_t size;
-
-	// T_FILE:
-	FILE *f;
+	void *data;
+	writefunc_t *write;
+	freefunc_t *free;
 } t;
+
+// Writes n bytes from data to w.
+// Returns the number of bytes written.
+pub int write(t *w, const char *data, size_t n) {
+	int r = w->write(w->data, data, n);
+	if (r > 0) w->nwritten += r;
+	return r;
+}
+
+pub void free(t *w) {
+	if (w->free) w->free(w->data);
+}
+
+pub int writebyte(t *w, char b) {
+	return write(w, &b, 1);
+}
+
 
 t *_stdout = NULL;
 
@@ -27,48 +37,42 @@ pub t *stdout() {
 	return _stdout;
 }
 
-// Returns a writer to a static buffer.
-// When the buffer's place runs out, write will return EOF.
-pub t *static_buffer(char *data, size_t size) {
-	t *w = calloc(1, sizeof(t));
-	if (!w) panic("calloc failed");
-	w->type = T_STATIC;
-	w->data = data;
-	w->size = size;
-	return w;
-}
-
 // Returns a writer to a file.
 pub t *file(FILE *f) {
 	t *w = calloc(1, sizeof(t));
 	if (!w) panic("calloc failed");
-	w->type = T_FILE;
-	w->f = f;
+	w->data = f;
+	w->write = f_write;
 	return w;
 }
 
-pub void free(t *w) {
-	OS.free(w);
-}
-
-pub int writebyte(t *w, char b) {
-	return write(w, &b, 1);
-}
-
-// Writes n bytes from data to w.
-// Returns the number of bytes written.
-pub int write(t *w, const char *data, size_t n) {
-	if (w->type == T_STATIC) {
-		int r = st_write(w, data, n);
-		if (r > 0) w->nwritten += r;
-		return r;
-	}
-	size_t r = fwrite(data, 1, n, w->f);
-	w->nwritten += r;
+int f_write(void *ctx, const char *data, size_t n) {
+	FILE *f = ctx;
+	size_t r = fwrite(data, 1, n, f);
 	return (int) r;
 }
 
-int st_write(t *w, const char *data, size_t n) {
+
+typedef { char *data; size_t pos, size; } st_t;
+
+// Returns a writer to a static buffer.
+// When the buffer's place runs out, write will return EOF.
+pub t *static_buffer(char *data, size_t size) {
+	t *w = calloc(1, sizeof(t));
+	st_t *s = calloc(1, sizeof(st_t));
+	if (!w || !s) panic("calloc failed");
+
+	s->data = data;
+	s->size = size;
+
+	w->write = st_write;
+	w->free = OS.free;
+	w->data = s;
+	return w;
+}
+
+int st_write(void *ctx, const char *data, size_t n) {
+	st_t *w = ctx;
 	if (w->pos >= w->size) {
 		return EOF;
 	}
