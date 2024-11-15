52fc67caa10b0590f5e3eb21b244998d82e172bb
gasweld <gasweld@gmail.com>
2024-11-16 01:34:58 +0200

    thorn: extract the render

--- prog/thorn/thorn.c
+++ prog/thorn/thorn.c
@@ -4,22 +4,27 @@
 const double PI = 3.141592653589793;
 
 int main() {
-    srand(time(NULL));
-
     const int width = 1000;
     const int height = 1000;
-    double cr = (rand() % 10000) / 500.0 - 10; // -10 -> 10;
+
+	image.image_t *img = image.new(width, height);
+	thorn(img);
+	png.writeimg(img, "thorn.png");
+	image.free(img);
+	return 0;
+}
+
+void thorn(image.image_t *img) {
+	srand(time(NULL));
+	double cr = (rand() % 10000) / 500.0 - 10; // -10 -> 10;
     double ci = (rand() % 10000) / 500.0 - 10;
     double xmin = -PI;
     double xmax =  PI;
     double ymin = -PI;
     double ymax =  PI;
-
-	image.image_t *img0 = image.new(width, height);
+	int width = img->width;
+	int height = img->height;
 	image.rgb_t rgb;
-	
-
-    uint8_t *values = calloc(width*height, sizeof(uint8_t));
     for (int i = 0; i < width; i++) {
         // map i=[0..width] to zr=[xmin..xmax]
         double zr = xmin + i * (xmax - xmin) / width;
@@ -27,18 +32,12 @@ int main() {
             // map j=[0..height] to zi=[ymin..ymax]
             double zi = ymin + j * (ymax - ymin) / height;
 			double val = get_sample(ci, cr, zi, zr);
-            values[j*width+i] = val;
-
 			rgb.red = (int) val;
 			rgb.green = (int) val;
 			rgb.blue = (int) val;
-			image.set(img0, i, j, rgb);
+			image.set(img, i, j, rgb);
         }
     }
-
-	png.writeimg(img0, "thorn.png");
-	image.free(img0);
-	return 0;
 }
 
 uint8_t get_sample(double ci, cr, zi, zr) {
