34dfca12652a635c120fd1baeb67ab7d995cb5a8
gasweld <gasweld@gmail.com>
2025-01-23 02:06:12 +0200

    map: unlimit the entries number

--- lib/clip/map.c
+++ lib/clip/map.c
@@ -1,7 +1,18 @@
+typedef {
+	// Each entry has its own key size.
+	size_t keysize;
+	uint8_t key[8];
+
+	// Value, the size is constant for the map instance.
+	uint8_t value[8];
+} entry_t;
+
 pub typedef {
-	int size;
-	const char *keys[200];
-	uint64_t values[200];
+	size_t cap;
+	size_t size;
+	void *entries;
+
+	// Value size in bytes.
 	size_t valsize;
 } map_t;
 
@@ -14,53 +25,69 @@ pub map_t *new(size_t valsize) {
 	map_t *m = calloc(1, sizeof(map_t));
 	if (!m) panic("calloc failed");
 	m->valsize = valsize;
+
+	m->cap = 16;
+	m->entries = calloc(m->cap, sizeof(entry_t));
+	if (!m->entries) panic("calloc failed");
+
 	return m;
 }
 
 // Frees the given instance of map.
 pub void free(map_t *m) {
+	OS.free(m->entries);
 	OS.free(m);
 }
 
 // Assigns a value to the given key.
 pub void set(map_t *m, const char *key, void *val) {
-	int pos = find(m, key);
-	if (pos < 0) {
-		if (m->size == 200) {
-			panic("max map size reached");
-		}
-		pos = m->size++;
-	}
-	m->keys[pos] = key;
-	memcpy(&m->values[pos], val, m->valsize);
+	entry_t *e = find(m, key);
+	if (!e) e = create(m, key);
+	memcpy(e->value, val, m->valsize);
 }
 
 // Copies the value stored at the given key into the buffer val.
 // If the value doesn't exist, doesn't modify val and returns false.
 pub bool get(map_t *m, const char *key, void *val) {
-	int pos = find(m, key);
-	if (pos < 0) {
-		return false;
-	}
-	memcpy(val, &m->values[pos], m->valsize);
-	return m->values[pos];
+	entry_t *e = find(m, key);
+	if (!e) return false;
+	memcpy(val, e->value, m->valsize);
+	return true;
 }
 
 // Returns true if the map has the entry with the given key.
 pub bool has(map_t *m, const char *key) {
-	return find(m, key) >= 0;
+	return find(m, key) != NULL;
 }
 
-int find(map_t *m, const char *key) {
-	for (int i = 0; i < m->size; i++) {
-		if (!strcmp(key, m->keys[i])) {
-			return i;
+entry_t *find(map_t *m, const char *key) {
+	entry_t *ee = m->entries;
+	for (size_t i = 0; i < m->size; i++) {
+		entry_t *e = &ee[i];
+		if (!strcmp(key, (char *)e->key)) {
+			return e;
 		}
 	}
-	return -1;
+	return NULL;
 }
 
-pub typedef { map_t *map; int pos; } iter_t;
+entry_t *create(map_t *m, const char *key) {
+	if (strlen(key) > 7) {
+		panic("key too long");
+	}
+	if (m->size == m->cap) {
+		m->cap *= 2;
+		m->entries = realloc(m->entries, m->cap * sizeof(entry_t));
+		if (!m->entries) panic("realloc failed");
+	}
+	entry_t *ee = m->entries;
+	entry_t *e = &ee[m->size++];
+	memset(e, 0, sizeof(entry_t));
+	memcpy(e->key, key, strlen(key));
+	return e;
+}
+
+pub typedef { map_t *map; size_t pos; } iter_t;
 
 pub iter_t *iter(map_t *m) {
 	iter_t *it = calloc(1, sizeof(iter_t));
@@ -79,11 +106,15 @@ pub bool next(iter_t *it) {
 }
 
 pub const char *itkey(iter_t *it) {
-	return it->map->keys[it->pos];
+	entry_t *ee = it->map->entries;
+	const char *k = (char *)ee[it->pos].key;
+	return k;
 }
 
 pub void itval(iter_t *it, void *val) {
-	memcpy(val, &it->map->values[it->pos], it->map->valsize);
+	entry_t *ee = it->map->entries;
+	entry_t *e = &ee[it->pos];
+	memcpy(val, e->value, it->map->valsize);
 }
 
 pub void end(iter_t *it) {
--- lib/clip/map.test.c
+++ lib/clip/map.test.c
@@ -14,5 +14,20 @@ int main() {
 	test.truth("get", map.get(m, "a", &val));
 	test.truth("a = 123456", val == 123456);
 
+	val = 111;
+	map.set(m, "a", &val);
+	test.truth("get", map.get(m, "a", &val));
+	test.truth("a = 111", val == 111);
+
+	char key[8] = {};
+	for (int i = 32; i < 80; i++) {
+		key[0] = i;
+		map.set(m, key, &i);
+	}
+	for (int i = 32; i < 80; i++) {
+		key[0] = i;
+		test.truth("get", map.get(m, key, &val));
+		test.truth("[key-i] == i", val == i);
+	}
 	return test.fails();
 }
