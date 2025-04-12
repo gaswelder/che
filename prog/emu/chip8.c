a38f8ecb6d1c64abaa0a30c631613a769ef2124a
gasweld <gasweld@gmail.com>
2025-04-13 01:14:03 +0300

    Reuse the decoder in disas

--- prog/emu/chip8.c
+++ prog/emu/chip8.c
@@ -566,107 +566,159 @@ void draw(chip8_t *c8) {
 	}
 }
 
+void print_instr(instr_t i) {
+	switch (i.OP) {
+		case JPnnn: { fprintf(stdout, "JP 0x%x", i.nnn); }
+		case CALLnnn: { fprintf(stdout, "CALL 0x%x", i.nnn); }
+		case SEVxkk: { fprintf(stdout, "SE V%x 0x%x \t// skip if V[%x] == %d", i.x, i.kk, i.x, i.kk); }
+		case SNEVxkk: {
+			fprintf(stdout, "SNE V%x 0x%x \t// skip if V[%x] != %d", i.x, i.kk, i.x, i.kk);
+		}
+		case SEVxVy: {
+			fprintf(stdout, "SE V%x V%x \t// skip if equal", i.x, i.y);
+		}
+		case LDVxkk: {
+			fprintf(stdout, "LD V%x 0x%x \t// V[%x] = %d", i.x, i.kk, i.x, i.kk);
+		}
+		case ADDVxkk: {
+			fprintf(stdout, "ADD V%x 0x%x \t// V[%x] += %d", i.x, i.kk, i.x, i.kk);
+		}
+		case LDVxVy: {
+			fprintf(stdout, "LD V%x V%x \t// V[%x] = V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case ORVxVy: {
+			fprintf(stdout, "OR V%x V%x \t// V[%x] |= V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case ANDVxVy: {
+			fprintf(stdout, "AND V%x V%x \t// V[%x] &= V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case XORVxVy: {
+			fprintf(stdout, "XOR V%x V%x \t// V[%x] ^= V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case ADDVxVy: {
+			fprintf(stdout, "ADD V%x V%x \t// V[%x] += V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case SUBVxVy: {
+			fprintf(stdout, "SUB V%x V%x \t// V[%x] -= V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case SHRVx_Vy_: {
+			fprintf(stdout, "SHR Vx _Vy_");
+		}
+		case SUBNVxVy: {
+			fprintf(stdout, "SUBN Vx Vy");
+		}
+		case SHLVx_Vy_: {
+			fprintf(stdout, "SHL Vx _Vy_");
+		}
+		case SNEVxVy: {
+			fprintf(stdout, "SNE V%x V%x \t// skip if V[%x] != V[%x]", i.x, i.y, i.x, i.y);
+		}
+		case LDInnn: {
+			fprintf(stdout, "LD I 0x%x \t// I = %x", i.nnn, i.nnn);
+		}
+		case JPV0nnn: {
+			fprintf(stdout, "JP V0 nnn");
+		}
+		case RNDVxkk: {
+			fprintf(stdout, "RND V%x 0x%x \t// V[%x] = random & 0x%x", i.x, i.kk, i.x, i.kk);
+		}
+		case DRWVxVyn: {
+			fprintf(stdout, "DRW V%x V%x %x \t// draw %d bytes from I at (V[%x], V[%x])", i.x, i.y, i.d4, i.d4, i.x, i.y);
+		}
+		case SKPVx: {
+			fprintf(stdout, "SKP Vx");
+		}
+		case SKNPVx: {
+			fprintf(stdout, "SKNP Vx");
+		}
+		case LDVxDT: {
+			fprintf(stdout, "LD Vx DT");
+		}
+		case LDVxK: {
+			fprintf(stdout, "LD Vx K");
+		}
+		case LDDTVx: {
+			fprintf(stdout, "LD DT Vx");
+		}
+		case LDSTVx: {
+			fprintf(stdout, "LD ST Vx");
+		}
+		case ADDIVx: {
+			fprintf(stdout, "ADD I Vx");
+		}
+		case LDFVx: {
+			fprintf(stdout, "LD F Vx");
+		}
+		case LDBVx: {
+			fprintf(stdout, "LDBVx");
+		}
+		case LD_I_Vx: {
+			fprintf(stdout, "LD_I_Vx");
+		}
+		case LDVx_I_: {
+			fprintf(stdout, "LD V%x [I] \t// init V[0]..V[%x] from memory at I", i.x, i.x);
+		}
+		case RET: { fprintf(stdout, "RET\n"); }
+		case CLS: { fprintf(stdout, "CLS"); }
+		default: {
+			fprintf(stdout, "unknown op %x", i.OP);
+		}
+	}
+}
+
 pub int disas(char *rompath) {
+	// Read and parse the whole thing.
+	instr_t img[4000];
+	size_t n = 0;
 	FILE *in = fopen(rompath, "rb");
 	if (!in) {
 		fprintf(stderr, "Couldn't open input file %s: %s\n", rompath, strerror(errno));
 		return 1;
 	}
-	size_t pos = 0x200;
 	while (!feof(in)) {
-		fprintf(stdout, "0x%zu\t", pos);
+		int a = fgetc(in);
+		int b = fgetc(in);
+		if (a == EOF && b == EOF) break;
+		if (b == EOF) {
+			fprintf(stderr, "unexpected end of file\n");
+			return 1;
+		}
+		uint8_t byte1 = (uint8_t) a;
+		uint8_t byte2 = (uint8_t) b;
+		decode(&img[n], byte1, byte2);
+		n++;
+	}
+	fclose(in);
 
-		uint8_t byte1 = 0;
-		uint8_t byte2 = 0;
-		fread( &byte1, 1, 1, in );
-		fread( &byte2, 1, 1, in );
+	size_t call_targets[100] = {};
+	size_t call_targets_len = 0;
+	for (size_t j = 0; j < n; j++) {
+		instr_t i = img[j];
+		if (i.OP == CALLnnn) {
+			call_targets[call_targets_len++] = i.nnn;
+		}
+	}
+
+	// Print
+	size_t pos = MEM_BEGIN;
+	for (size_t j = 0; j < n; j++) {
+		fprintf(stdout, "0x%x\t", (int) pos);
+		instr_t i = img[j];
+		print_instr(i);
+		fprintf(stdout, "\n");
 		pos += 2;
-
-		uint8_t a = (byte1 & (0xF << 4)) >> 4;
-		uint8_t b = (byte1 & 0xF);
-		uint8_t c = (byte2 & (0xF << 4)) >> 4;
-		uint8_t d = (byte2 & 0xF);
-		uint16_t param2 = (c << 4) + d;
-		uint16_t param3 = (b << 8) + (c << 4) + d;
-
-		fprintf( stdout, "%x%x%x%x\t", a, b, c, d );
-
-		switch (a) {
-			case 0x0: {
-				if (b == 0x0 && c == 0xE) {
-					switch (d) {
-						case 0x0: { fprintf( stdout, "CLS\n" ); }
-						case 0xE: { fprintf( stdout, "RET\n" ); }
-						default: { fprintf(stdout, "Unknown\n" ); return 2; }
-					}
-				} else {
-					fprintf( stdout, "SYS\t0x%x\n", param3 );
-				}
-			}
-			case 0x1: { fprintf( stdout, "JP\t0x%x\n", param3 ); }
-			case 0x2: { fprintf( stdout, "CALL\t0x%x\n", param3 ); }
-			case 0x3: { fprintf( stdout, "SE\tV%x, 0x%x\n", b, param2 ); }
-			case 0x4: { fprintf( stdout, "SNE\tV%x, 0x%x\n", b, param2 ); }
-			case 0x5: {
-				if (d == 0) {
-					fprintf(stdout, "SE\tV%x, V%x\n", b, c);
-				} else {
-					fprintf(stdout, "Unknown\n");
-					return 2;
-				}
-			}
-			case 0x6: { fprintf( stdout, "LD\tV%x, 0x%x\n", b, param2 ); }
-			case 0x7: { fprintf( stdout, "ADD\tV%x, 0x%x\n", b, param2 ); }
-			case 0x8: {
-				switch (d) {
-					case 0x0: { fprintf( stdout, "LD\tV%x, V%x\n", b, c ); }
-					case 0x1: { fprintf( stdout, "OR\tV%x, V%x\n", b, c ); }
-					case 0x2: { fprintf( stdout, "AND\tV%x, V%x\n", b, c ); }
-					case 0x3: { fprintf( stdout, "XOR\tV%x, V%x\n", b, c ); }
-					case 0x4: { fprintf( stdout, "ADD\tV%x, V%x\n", b, c ); }
-					case 0x5: { fprintf( stdout, "SUB\tV%x, V%x\n", b, c ); }
-					case 0x6: { fprintf( stdout, "SHR\tV%x {, V%x}\n", b, c ); }
-					case 0x7: { fprintf( stdout, "SUBN\tV%x, V%x\n", b, c ); }
-					case 0xE: { fprintf( stdout, "SHL\tV%x {, V%x}\n", b, c ); }
-				}
-			}
-			case 0x9: {
-				assert( d == 0 );
-				fprintf( stdout, "SNE\tV%x, V%x\n", b, c );
-			}
-			case 0xA: { fprintf( stdout, "LD\tI, 0x%x\n", param3 ); }
-			case 0xB: { fprintf( stdout, "JP\tV0, 0x%x\n", param3 ); }
-			case 0xC: { fprintf( stdout, "RND\tV%x, 0x%x\n", b, param2 ); }
-			case 0xD: { fprintf( stdout, "DRW\tV%x, V%x, %d\n", b, c, d ); }
-			case 0xE: {
-				if( c == 0x9 && d == 0xE ){
-					fprintf( stdout, "SKP\tV%x\n", b );
-					break;
-				}
-				if( c == 0xA && d == 0x1 ){
-					fprintf( stdout, "SNKP\tV%x\n", b );
-					break;
-				}
-				fprintf( stdout, "Error\n" );
-				return 2;
-			}
-			case 0xF: {
-				switch ((c<<4) + d) {
-					case 0x07: { fprintf( stdout, "LD\tV%x, DT\n", b ); }
-					case 0x0A: { fprintf( stdout, "LD\tV%x, K\n", b ); }
-					case 0x15: { fprintf( stdout, "LD\tDT, V%x\n", b ); }
-					case 0x18: { fprintf( stdout, "LD\tST, V%x\n", b ); }
-					case 0x1E: { fprintf( stdout, "ADD\tI, V%x\n", b ); }
-					case 0x29: { fprintf( stdout, "LD\tF, V%x\n", b ); }
-					case 0x33: { fprintf( stdout, "LD\tB, V%x\n", b ); }
-					case 0x55: { fprintf( stdout, "LD\t[I], V%x\n", b ); }
-					case 0x65: { fprintf( stdout, "LD\tV%x, [I]\n", b ); }
-				}
-			}
-			default: {
-				fprintf(stderr, "Unknown: %02x %02x\n", byte1, byte2 );
-			}
+		if (contains(call_targets, call_targets_len, pos)) {
+			fprintf(stdout, "\n ----- proc ----- \n");
 		}
 	}
 	return 0;
+}
+
+bool contains(size_t *xs, size_t n, size_t x) {
+	for (size_t i = 0; i < n; i++) {
+		if (xs[i] == x) {
+			return true;
+		}
+	}
+	return false;
 }
