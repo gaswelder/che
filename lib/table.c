53848398bbac9b64fb0a39f040caf35e533712af
gasweld <gasweld@gmail.com>
2025-08-15 01:06:33 +0300

    Extract a new table printer

--- lib/table.c
+++ lib/table.c
@@ -1,35 +1,80 @@
 pub typedef {
-    char left[1000];
-    char right[1000];
-} item_t;
+	size_t ncols;
+	size_t pos;
+	char **values;
+	size_t cap;
+} table_t;
 
-pub typedef {
-    item_t items[1000];
-    size_t nitems;
-} t;
-
-pub void add(t *tbl, const char *title, const char *fmt, ...) {
-    tbl->nitems++;
-    item_t *it = &tbl->items[tbl->nitems];
-    strcpy(it->left, title);
-    
-    va_list l = {0};
-	va_start(l, fmt);
-	vsprintf(it->right, fmt, l);
-	va_end(l);
+pub table_t *new(size_t ncols) {
+	table_t *t = calloc!(1, sizeof(table_t));
+	t->ncols = ncols;
+	t->cap = 100;
+	t->values = calloc!(ncols * 100, sizeof(char *));
+	return t;
 }
 
-pub void split(t *tbl) {
-    tbl->nitems++;
+pub void pushval(table_t *t, char *format, ...) {
+	if (t->pos == t->cap) {
+		t->cap += 100;
+		char **n = realloc(t->values, t->cap * sizeof(table_t));
+		if (!n) panic("realloc failed");
+		t->values = n;
+	}
+	va_list args = {};
+	va_start(args, format);
+	int len = vsnprintf(NULL, 0, format, args);
+	va_end(args);
+	if (len < 0) {
+		panic("failed to format the string");
+	}
+	char *s = calloc!(len + 1, 1);
+	va_start(args, format);
+	vsnprintf(s, len + 1, format, args);
+	va_end(args);
+
+	t->values[t->pos++] = s;
 }
 
-pub void print(t *tbl) {
-    for (size_t i = 0; i < tbl->nitems; i++) {
-        item_t *it = &tbl->items[i];
-        if (!it->left[0]) {
-            printf("\n");
-            continue;
-        }
-        printf("%-30s%s\n", it->left, it->right);
-    }
+pub void print(table_t *t) {
+	size_t *widths = calloc!(t->ncols, sizeof(size_t));
+
+	for (size_t i = 0; i < t->pos; i++) {
+		size_t col = i % t->ncols;
+		size_t len = strlen(t->values[i]);
+		if (len > widths[col]) {
+			widths[col] = len + 2;
+		}
+	}
+
+	for (size_t i = 0; i < t->pos; i++) {
+		size_t col = i % t->ncols;
+		printwidth(t->values[i], widths[col]);
+		if (col == t->ncols-1) {
+			putchar('\n');
+		}
+	}
+
+	OS.free(widths);
+}
+
+pub void free(table_t *t) {
+	for (size_t i = 0; i < t->pos; i++) {
+		OS.free(t->values[i]);
+	}
+	OS.free(t->values);
+	OS.free(t);
+}
+
+void printwidth(char *s, size_t w) {
+	size_t n = 0;
+	while (*s != '\0') {
+		putchar(*s);
+		s++;
+		n++;
+		if (n == w) break;
+	}
+	while (n < w) {
+		putchar(' ');
+		n++;
+	}
 }
--- prog/floatbits/floatbits.c
+++ prog/floatbits/floatbits.c
@@ -1,51 +1,92 @@
 #import bitreader
+#import table
 
 // prints bits of float64 numbers.
 int main() {
-	printfloat(-OS.INFINITY);
-	printfloat(OS.INFINITY);
-	printfloat(0);
-	printfloat(1);
-	printfloat(2);
-	printfloat(1024);
-	printfloat(0.25);
-	printfloat(0.25 + 0.00000000000000005);
-	printfloat(100000);
+	table.table_t *tbl = table.new(4);
+	table.pushval(tbl, "value");
+	table.pushval(tbl, "sign");
+	table.pushval(tbl, "exponent");
+	table.pushval(tbl, "mantissa");
+
+	printfloat(tbl, -OS.INFINITY);
+	printfloat(tbl, OS.INFINITY);
+	printfloat(tbl, 0);
+	printfloat(tbl, 1);
+	printfloat(tbl, 2);
+	printfloat(tbl, 1024);
+	printfloat(tbl, 0.25);
+	printfloat(tbl, 0.25 + 0.00000000000000005);
+	printfloat(tbl, 100000);
+
+	table.print(tbl);
+	table.free(tbl);
 	return 0;
 }
 
-void printfloat(double x) {
+void printbits(char *buf, uint8_t *bits, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		if (bits[i] == 1) {
+			buf[i] = '1';
+		} else {
+			buf[i] = '0';
+		}
+	}
+}
+
+void printfloat(table.table_t *tbl, double x) {
+	parsed_double_t parsed = parse_double(x);
+	char buf[64] = {};
+	table.pushval(tbl, "%30.20f", x);
+	table.pushval(tbl, "%d", parsed.sign);
+
+	printbits(buf, parsed.exp_bits, 11);
+	table.pushval(tbl, "[%s] (%d)", buf, parsed.exp);
+
+	printbits(buf, parsed.sig_bits, 52);
+	table.pushval(tbl, "[%s] (%zu)", buf, parsed.sig);
+}
+
+typedef {
+	uint8_t bits[64];
+	uint8_t sign;
+	uint8_t exp_bits[11];
+	uint8_t sig_bits[52];
+	int exp;
+	uint64_t sig;
+} parsed_double_t;
+
+parsed_double_t parse_double(double x) {
+	parsed_double_t parsed = {};
+
 	// Look at x as a sequence of bytes.
 	void *p = &x;
 	uint8_t *bytes = p;
 
 	// Unpack the bits.
-	char bits[64] = {};
 	uint8_t bits8[8] = {};
 	for (int i = 0; i < 8; i++) {
 		bitreader.getbits_lsfirst(bytes[i], bits8);
 		for (int j = 0; j < 8; j++) {
-			bits[8*i + j] = bits8[j];
+			parsed.bits[8*i + j] = bits8[j];
 		}
 	}
 
-	printf("%30.20f\t", x);
-	printf("%d|", bits[63]);
+	// The leftmost bit is the sign.
+	parsed.sign = parsed.bits[63];
 
-	int exp = 0;
+	// Next 11 bits is the exponent.
 	for (int i = 62; i >= 52; i--) {
-		exp *= 2;
-		exp += bits[i];
-		printf("%d", bits[i]);
+		parsed.exp *= 2;
+		parsed.exp += parsed.bits[i];
+		parsed.exp_bits[(i-52)] = parsed.bits[i];
 	}
-	exp -= 1023;
-	printf(" = %5d|", exp);
+	parsed.exp -= 1023;
 
-	uint64_t sig = 0;
+	// The rest is the normalized value.
 	for (int i = 51; i >= 0; i--) {
-		sig *= 2;
-		sig += bits[i];
-		printf("%d", bits[i]);
+		parsed.sig *= 2;
+		parsed.sig += parsed.bits[i];
 	}
-	printf("=%zu\n", sig);
+	return parsed;
 }
