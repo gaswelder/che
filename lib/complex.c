7bf0a3d41ed24bb92b4c64167a589cd628df3dec
gasweld <gasweld@gmail.com>
2025-08-30 15:02:22 +0300

    Polish

--- lib/complex.c
+++ lib/complex.c
@@ -4,24 +4,40 @@ pub typedef {
 	double re, im;
 } t;
 
-/*
- * Returns the sum of a and b.
-*/
+// Returns the sum of complex a and b.
 pub t sum(t a, b) {
-	a.re += b.re;
-	a.im += b.im;
-	return a;
+	t r = {
+		.re = a.re + b.re,
+		.im = a.im + b.im
+	};
+	return r;
 }
 
-/*
- * Returns the product of a and b.
- */
+// Returns the difference of complex a-b.
+pub t diff(t a, b) {
+	t r = {
+		.re = a.re - b.re,
+		.im = a.im - b.im
+	};
+	return r;
+}
+
+// Returns the product of complex a and b.
 pub t mul(t a, b) {
 	t r = {
 		.re = a.re * b.re - a.im * b.im,
 		.im = a.re * b.im + b.re * a.im
 	};
 	return r;	
+}
+
+// Multiplies complex x by scalar s.
+pub t scale(t x, double s) {
+	t r = {
+		.re = x.re * s,
+		.im = x.im * s
+	};
+	return r;
 }
 
 pub double abs(t z) {
