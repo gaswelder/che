0c1f617745fcd45f596063d8bf3ded08e92715ff
gasweld <gasweld@gmail.com>
2025-08-31 16:01:07 +0300

    Regroup more code

--- prog/futu/cmd_render.c
+++ prog/futu/cmd_render.c
@@ -32,56 +32,17 @@ pub int run(int argc, char *argv[]) {
 	const char *path = argv[1];
 	load_clips("list");
 
-	midireader_t r = {};
-	initreader(&r, path);
+	composer_t c = {};
+	cmpinit(&c, track, path);
 
-	vec.t *ranges = vec.new(sizeof(range_t));
-
-	while (true) {
-		midilib.event_t *e = next(&r);
-		if (!e) break;
-		if (e->track != track) continue;
-
-		switch (e->type) {
-			case midilib.NOTE_ON: {
-				range_t *r = vec.alloc(ranges);
-				r->t1 = e->t_us * 44100 / 1000000;
-				r->key = e->key;
-				clip_t *c = get_clip(e->key);
-				if (!c) {
-					fprintf(stderr, "no clip for key %u\n", e->key);
-				} else {
-					r->samples = c->clip->samples;
-					r->nsamples = c->clip->nsamples;
-				}
-			}
-			case midilib.NOTE_OFF: {
-				// Find the first unclosed matching range.
-				range_t *r = NULL;
-				size_t nranges = vec.len(ranges);
-				for (size_t i = 0; i < nranges; i++) {
-					range_t *x = vec.index(ranges, i);
-					if (x->key == e->key && x->t2 == 0) {
-						r = x;
-						break;
-					}
-				}
-				if (!r) {
-					panic("failed to find the open range to close");
-				}
-				r->t2 = e->t_us * 44100 / 1000000;
-				size_t tlen = r->t2 - r->t1;
-				if (r->nsamples > tlen) {
-					r->t2 = r->t1 + r->nsamples;
-					// fprintf(stderr, "trimmed sample %u: %zu vs %zu\n", r->key, tlen, r->nsamples);
-				}
-			}
-		}
+	wav.writer_t *out = wav.open_writer(stdout);
+	sound.samplef_t s = {};
+	while (cmpnext(&c, &s)) {
+		wav.write_sample(out, s.left, s.right);
 	}
+	wav.close_writer(out);
 
-	freereader(&r);
-	writeout(ranges);
-	free(ranges);
+	cmpdone(&c);
 	return 0;
 }
 
@@ -151,32 +112,95 @@ void load_clips(char *path) {
 	fclose(f);
 }
 
-void writeout(vec.t *ranges) {
-	size_t nranges = vec.len(ranges);
+typedef {
+	size_t nranges;
+	size_t tmax;
+	size_t i;
+	vec.t *ranges;
+} composer_t;
+
+void cmpinit(composer_t *c, uint8_t track, const char *path) {
+	midireader_t r = {};
+	initreader(&r, path);
+
+	c->ranges = vec.new(sizeof(range_t));
+
+	while (true) {
+		midilib.event_t *e = next(&r);
+		if (!e) break;
+		if (e->track != track) continue;
+
+		switch (e->type) {
+			case midilib.NOTE_ON: {
+				range_t *r = vec.alloc(c->ranges);
+				r->t1 = e->t_us * 44100 / 1000000;
+				r->key = e->key;
+				clip_t *c = get_clip(e->key);
+				if (!c) {
+					fprintf(stderr, "no clip for key %u\n", e->key);
+				} else {
+					r->samples = c->clip->samples;
+					r->nsamples = c->clip->nsamples;
+				}
+			}
+			case midilib.NOTE_OFF: {
+				// Find the first unclosed matching range.
+				range_t *r = NULL;
+				size_t nranges = vec.len(c->ranges);
+				for (size_t i = 0; i < nranges; i++) {
+					range_t *x = vec.index(c->ranges, i);
+					if (x->key == e->key && x->t2 == 0) {
+						r = x;
+						break;
+					}
+				}
+				if (!r) {
+					panic("failed to find the open range to close");
+				}
+				r->t2 = e->t_us * 44100 / 1000000;
+				size_t tlen = r->t2 - r->t1;
+				if (r->nsamples > tlen) {
+					r->t2 = r->t1 + r->nsamples;
+					// fprintf(stderr, "trimmed sample %u: %zu vs %zu\n", r->key, tlen, r->nsamples);
+				}
+			}
+		}
+	}
+
+	freereader(&r);
+	c->nranges = vec.len(c->ranges);
 	size_t tmax = 0;
-	for (size_t i = 0; i < nranges; i++) {
-		range_t *r = vec.index(ranges, i);
+	for (size_t i = 0; i < c->nranges; i++) {
+		range_t *r = vec.index(c->ranges, i);
 		// fprintf(stderr, "%zu: %zu..%zu: %u\n", i, r->t1, r->t2, r->key);
 		if (r->t2 > tmax) {
 			tmax = r->t2;
 		}
 	}
+	c->tmax = tmax;
+}
 
-	wav.writer_t *out = wav.open_writer(stdout);
+bool cmpnext(composer_t *c, sound.samplef_t *s) {
+	if (c->i == c->tmax) return false;
 	// This is a brute-force scan from 0 to the max time,
 	// where for each time instant we query all ranges.
-	for (size_t i = 0; i <= tmax; i++) {
-		double left = 0;
-		double right = 0;
-		for (size_t j = 0; j < nranges; j++) {
-			range_t *r = vec.index(ranges, j);
-			if (i < r->t1 || i >= r->t2) continue;
-			size_t pos = i - r->t1;
-			if (pos >= r->nsamples) continue;
-			left += r->samples[pos].left;
-			right += r->samples[pos].right;
-		}
-		wav.write_sample(out, left, right);
+	double left = 0;
+	double right = 0;
+	for (size_t j = 0; j < c->nranges; j++) {
+		range_t *r = vec.index(c->ranges, j);
+		if (c->i < r->t1 || c->i >= r->t2) continue;
+		size_t pos = c->i - r->t1;
+		if (pos >= r->nsamples) continue;
+		left += r->samples[pos].left;
+		right += r->samples[pos].right;
 	}
-	wav.close_writer(out);
+	s->left = left;
+	s->right = right;
+	c->i++;
+	return true;
+}
+
+
+void cmpdone(composer_t *c) {
+	free(c->ranges);
 }
