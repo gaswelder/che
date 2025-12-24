3f9a2ce0fe1bc61842cc0f4dad98f3fd8867fa20
gasweld <gasweld@gmail.com>
2025-12-24 20:34:07 +0200

    Tweak hist

--- prog/hist/hist.c
+++ prog/hist/hist.c
@@ -1,13 +1,8 @@
 #import stats
 #import opt
 
-typedef {
-	double a, b;
-	size_t count;
-} bin_t;
-
 int main(int argc, char *argv[]) {
-	size_t nbins = 0;
+	size_t nbins = 10;
 	size_t maxline = 30;
 	bool raw = false;
 	opt.nargs(0, "");
@@ -16,15 +11,23 @@ int main(int argc, char *argv[]) {
 	opt.size("w", "max line width", &maxline);
 	opt.flag("r", "print raw numbers, no fancy ASCII art", &raw);
 	opt.parse(argc, argv);
-
 	if (nbins != 0 && nbins < 2) {
 		fprintf(stderr, "The number of bins must be greater than two\n");
 		return 1;
 	}
+	stats.series_t *s = collect();
 
-	//
-	// Collect the values.
-	//
+	bin_t *bins = group_bins(s, nbins);
+	if (raw) {
+		printbins(bins, nbins);
+	} else {
+		printbins_ascii(bins, nbins, maxline);
+	}
+	return 0;
+}
+
+// Reads input numbers, returns them as series.
+stats.series_t *collect() {
 	stats.series_t *s = stats.newseries();
 	double val = 0;
 	while (true) {
@@ -32,66 +35,80 @@ int main(int argc, char *argv[]) {
 		if (r == EOF) break;
 		if (r != 1) {
 			fprintf(stderr, "failed to parse a number\n");
-			return 1;
+			exit(1);
 		}
 		stats.add(s, val);
 	}
+	return s;
+}
 
-	if (nbins == 0) {
-		nbins = (size_t) sqrt(s->len);
+typedef {
+	double minval;
+	size_t count;
+} bin_t;
+
+typedef {
+	double min, step;
+} range_t;
+
+range_t find_range(double _min, _max, size_t nbins) {
+	double scale = pow(10, floor(log10(fabs(_min))));
+	if (scale == 0) {
+		scale = 0.000001;
 	}
+	double amin = 0;
+	double amax = 0;
+	while (true) {
+		amin = floor(_min / scale) * scale;
+		amax = amin + nbins * scale;
+		if (amax >= _max) {
+			break;
+		}
+		scale *= 10;
+	}
+	while (amin + nbins * (scale/2) >= _max) {
+		scale /= 2;
+	}
+	range_t r = { .min = amin, .step = scale };
+	return r;
+}
 
-	//
-	// Group into bins.
-	//
-	bin_t *bins = calloc!(nbins+2, sizeof(bin_t));
-	double _min = stats.min(s);
-	double _max = stats.max(s);
-	double step = (_max - _min) / (nbins-1);
+bin_t *group_bins(stats.series_t *s, size_t nbins) {
+	range_t r = find_range(stats.min(s), stats.max(s), nbins);
 
-	// 1 bin for >= MIN-STEP
-	bins[0].a = _min - step;
-	bins[0].b = _min;
-
-	// n bins for >=MIN, >=MIN+STEP, ...
+	bin_t *bins = calloc!(nbins, sizeof(bin_t));
 	for (size_t i = 0; i < nbins; i++) {
-		bins[i+1].a = _min + i*step;
-		bins[i+1].b = _min + (i+1)*step;
+		bins[i].minval = r.min + i * r.step;
 	}
-
-	// 1 bin for >= MAX+STEP
-	bins[nbins+1].a = _max + step;
-	bins[nbins+1].b = _max + step + step;
 
 	for (size_t i = 0; i < s->len; i++) {
 		double x = s->values[i];
-
-		// This weird search is because the max value doesn't
-		// always "match" the last bin because of the floating
-		// point behavior.
-		bin_t *b = NULL;
-		for (size_t pos = 0; pos < nbins+2; pos++) {
-			b = &bins[pos];
-			if (x >= b->a && x < b->b) {
-				break;
-			}
+		bin_t *b = findbin(bins, nbins, x);
+		if (!b) {
+			panic("failed to find bin for %g", x);
 		}
 		b->count++;
 	}
+	return bins;
+}
 
-	if (raw) {
-		printbins(bins, nbins+2);
-	} else {
-		printbins_ascii(bins, nbins+2, maxline);
+bin_t *findbin(bin_t *bins, size_t nbins, double val) {
+	bin_t *b = NULL;
+	for (size_t i = 0; i < nbins; i++) {
+		bin_t *next = &bins[i];
+		if (val >= next->minval) {
+			b = next;
+		} else {
+			break;
+		}
 	}
-
-	return 0;
+	return b;
 }
 
 void printbins(bin_t *bins, size_t nbins) {
 	for (size_t i = 0; i < nbins; i++) {
 		bin_t *b = &bins[i];
-		printf("%f\t%zu\n", (b->a + b->b)/2, b->count);
+		printf("%g\t%zu\n", b->minval, b->count);
 	}
 }
 
@@ -109,7 +126,7 @@ void printbins_ascii(bin_t *bins, size_t nbins, size_t maxline) {
 	for (size_t i = 0; i < nbins; i++) {
 		bin_t *b = &bins[i];
 		label_t *l = &labels[i];
-		size_t n = snprintf(l->str, sizeof(l->str), "%f", b->a);
+		size_t n = snprintf(l->str, sizeof(l->str), "%g", b->minval);
 		if (n > maxlabel) maxlabel = n;
 	}
 
@@ -127,9 +144,14 @@ void printbins_ascii(bin_t *bins, size_t nbins, size_t maxline) {
 	//
 	// Header.
 	//
-	printf("value ");
-	repeat("-", maxlabel + maxline - strlen("value") + 1);
+	printf("value (>=)");
+	repeat("-", maxlabel + maxline - strlen("value (>=)") + 1);
 	printf(" count\n");
+
+	double scale = 30 / (double) maxcount;
+	if (scale > 1) {
+		scale = 1;
+	}
 
 	//
 	// Output the bin lines.
@@ -147,9 +169,7 @@ void printbins_ascii(bin_t *bins, size_t nbins, size_t maxline) {
 		// bar
 		//
 		size_t len = 0;
-		if (maxcount > 0 && b->count > 0) {
-			len = b->count * maxline / maxcount;
-		}
+		len = (size_t) ((double)b->count * scale);
 		repeat("█", len);
 		repeat(" ", maxline-len);
 
