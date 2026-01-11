2aabd8061d7ade00c8b12e67fee2759c4ce9a4fc
gasweld <gasweld@gmail.com>
2026-01-11 02:03:31 +0200

    Add a frequency parameter

--- prog/woo/woo.c
+++ prog/woo/woo.c
@@ -1,11 +1,17 @@
 #import formats/wav
+#import opt
 
-int main() {
+int main(int argc, char *argv[]) {
+	size_t freq = 1200;
+	opt.size("f", "tone frequency in Hz", &freq);
+	opt.nargs(0, "");
+	opt.parse(argc, argv);
+
 	wav.writer_t *w = wav.open_writer(stdout);
 	int nsamples = 44100 / 10;
-	float hz = 1000;
+	float hz = (float) freq;
 	for (int j = 0; j < nsamples; j++) {
-		float v = sinf(j * 2.0f * M_PI / 44100 * hz); // * envelope;
+		float v = sinf(j * 2.0f * M_PI / 44100 * hz);
 		wav.write_sample(w, v, v);
 	}
 	wav.close_writer(w);
