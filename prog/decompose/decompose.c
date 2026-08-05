5051b1f178212d22bb26c7b3a6702ddca000c413
gasweld <gasweld@gmail.com>
2026-08-06 01:33:46 +0300

    Fix decompose parsing

--- prog/decompose/decompose.c
+++ prog/decompose/decompose.c
@@ -3,25 +3,39 @@
 #import linereader
 
 int main() {
+    OS.setvbuf(stdout, NULL, OS._IOLBF, 0);
 	linereader.t *lr = linereader.new(stdin);
     while (linereader.read(lr)) {
         char *p = linereader.line(lr);
 
         // color, container name, spaces, "|", ESC[0m
 		p = termcolor(p);
-        while (!isspace(*p)) p++;
+
+        char name[100] = {};
+        int len = 0;
+        while (!isspace(*p)) {
+            name[len++] = *p++;
+        }
+
         while (isspace(*p)) p++;
+
         if (*p == '|') p++;
         p = termcolor(p);
+        while (isspace(*p)) p++;
 
-		// message
-        printf("%s", p);
+        if (p[0] == '{') {
+            printf("{\"container\":\"%s\",", name);
+            printf("%s", &p[1]);
+        } else {
+            printf("[%s] %s", name, p);
+        }
     }
 	linereader.free(lr);
     return 0;
 }
 
 // ESC[0m
+// ESC[36;1m
 char *termcolor(char *p) {
     char *p0 = p;
 
@@ -32,6 +46,7 @@ char *termcolor(char *p) {
 	// num ";" num
     while (isdigit(*p)) p++;
     if (*p == ';') {
+        p++;
         while (isdigit(*p)) p++;
     }
 	// m
