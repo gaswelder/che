1f3ed14f70e687b09d4f42d82f574e897d329aef
gasweld <gasweld@gmail.com>
2026-08-24 19:17:01 +0300

    Include Info as Xing

--- lib/formats/mp3.c
+++ lib/formats/mp3.c
@@ -275,8 +275,7 @@ bool read_xing(reader.t *r, xing_header_t *x) {
     uint8_t magic[4];
     reader.read(r, magic, 4);
 
-    if (memcmp(magic, "Xing", 4) != 0) {
-		// Could also be "Info" but haven't encountered it.
+    if (memcmp(magic, "Xing", 4) != 0 && memcmp(magic, "Info", 4) != 0) {
         return false;
     }
 
