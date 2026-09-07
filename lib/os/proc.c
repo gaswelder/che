6a28f35adf31b99d65c5e5b60d51f6fa89f00921
gasweld <gasweld@gmail.com>
2026-09-08 01:40:42 +0300

    Drop parallel

--- lib/os/proc.c
+++ lib/os/proc.c
@@ -86,36 +86,3 @@ pub bool wait(proc_t *p, int *status) {
     free(p);
     return ok;
 }
-
-
-pub typedef int workerfunc_t(void *);
-
-/*
- * Processes the list of jobs using forking processes,
- * each process running f on its corresponding item of the jobs list.
- * jobs is a null-terminated list of pointers.
-*/
-pub void parallel(workerfunc_t f, void **jobs) {
-	int nworkers = 0;
-    void **job = jobs;
-	for (int i = 0; i < 8; i++) {
-        if (!*job) break;
-		spawnp(f, *job++);
-		nworkers++;
-	}
-	while (nworkers > 0) {
-		OS.wait(NULL);
-		nworkers--;
-		if (*job) {
-			spawnp(f, *job++);
-			nworkers++;
-		}
-	}
-}
-
-void spawnp(workerfunc_t f, void *job) {
-    int pid = OS.fork();
-    if (pid < 0) panic("fork failed");
-    if (pid > 0) return;
-	exit(f(job));
-}
